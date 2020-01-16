fbf1ea8c99008ef911bf801d4016b5dbee763d23
Dylan Freedman <freedmand@gmail.com>
2020-01-16 18:34:02 +0000

    Temporarily obscure bundleanalyzerplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-const autoPreprocess = require('svelte-preprocess');
-const { preprocessOptions } = require('./preprocess.config.js');
-const DotenvFlow = require('dotenv-flow-webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const autoPreprocess = require("svelte-preprocess");
+const { preprocessOptions } = require("./preprocess.config.js");
+const DotenvFlow = require("dotenv-flow-webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
-const mode = process.env.NODE_ENV || 'development';
-const prod = mode === 'production';
+const mode = process.env.NODE_ENV || "development";
+const prod = mode === "production";
 
 module.exports = {
   entry: {
-    bundle: ['./src/main.js']
+    bundle: ["./src/main.js"]
   },
   resolve: {
     alias: {
-      svelte: path.resolve('node_modules', 'svelte'),
-      '@': path.resolve(__dirname, 'src'),
+      svelte: path.resolve("node_modules", "svelte"),
+      "@": path.resolve(__dirname, "src")
     },
-    extensions: ['*', '.mjs', '.js', '.svelte', '.css', '.scss'],
-    mainFields: ['svelte', 'browser', 'module', 'main']
+    extensions: ["*", ".mjs", ".js", ".svelte", ".css", ".scss"],
+    mainFields: ["svelte", "browser", "module", "main"]
   },
   output: {
-    path: __dirname + '/public',
-    filename: '[name].js',
-    chunkFilename: '[name].[id].js'
+    path: __dirname + "/public",
+    filename: "[name].js",
+    chunkFilename: "[name].[id].js"
   },
   module: {
     rules: [
       {
         test: /\.svelte$/,
         use: {
-          loader: 'svelte-loader',
+          loader: "svelte-loader",
           options: {
             emitCss: true,
             hotReload: true,
@@ -46,8 +47,8 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
             options: { hmr: !prod }
           },
-          'css-loader',
-          'sass-loader',
+          "css-loader",
+          "sass-loader"
         ]
       },
       {
@@ -58,14 +59,14 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
             options: { hmr: !prod }
           },
-          'css-loader'
+          "css-loader"
         ]
       },
       {
         test: /\.svg$/,
         use: [
           {
-            loader: 'svg-inline-loader',
+            loader: "svg-inline-loader",
             options: { removeSVGTagAttrs: false }
           }
         ]
@@ -75,23 +76,23 @@ module.exports = {
   mode,
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'bundle.css'
+      filename: "bundle.css"
     }),
-    new DotenvFlow(),
-    ...(prod ? [new BundleAnalyzerPlugin({
-      analyzerPort: 80,
-      analyzerHost: '0.0.0.0',
-      openAnalyzer: false
-    })] : [])
+    new DotenvFlow()
+    // ...(prod ? [new BundleAnalyzerPlugin({
+    //   analyzerPort: 80,
+    //   analyzerHost: '0.0.0.0',
+    //   openAnalyzer: false
+    // })] : [])
   ],
-  devtool: prod ? false : 'source-map',
+  devtool: prod ? false : "source-map",
   devServer: {
     disableHostCheck: true,
-    host: '0.0.0.0',
-    public: '0.0.0.0:80',
+    host: "0.0.0.0",
+    public: "0.0.0.0:80",
     port: 80,
     historyApiFallback: {
-      index: 'index.html'
+      index: "index.html"
     }
   }
 };
