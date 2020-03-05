868c60a572946a02a83702e9c6b9350edcfd4649
Dylan Freedman <freedmand@gmail.com>
2020-03-05 02:05:17 +0000

    Home page copy changes

--- src/pages/home/Home.svelte
+++ src/pages/home/Home.svelte
@@ -2,14 +2,34 @@
   import HomeTemplate from "./HomeTemplate";
 </script>
 
+<style lang="scss">
+  a {
+    color: $primary;
+  }
+
+  .note {
+    background: $fyi;
+    border-radius: $radius;
+    padding: 5px 10px;
+    margin: 0 -10px;
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.12);
+  }
+</style>
+
 <HomeTemplate showMast="true">
   <p>
     DocumentCloud is an all-in-one platform for documents: upload, organize,
     analyze, annotate, search, and embed.
   </p>
-  <p>
-    Primarily used by journalists, DocumentCloud has established a reputation as
-    a store for primary source documents and has been utilized in breaking
-    investigative stories worldwide.
+  <p class="note">
+    Please note that this is currently an invite-only beta. You can
+    <a href="https://documentcloud.org">
+      log in to the classic version of DocumentCloud here.
+    </a>
+    To receive an invite to test the new version of the site, 
+    <a
+      href="https://documentcloud.us11.list-manage.com/subscribe?u=4e1ca6e52b3163639d641f360&id=20a793193a">
+      subscribe to our newsletter.
+    </a>
   </p>
 </HomeTemplate>
