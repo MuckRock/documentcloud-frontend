ece85dea64e2b5809a8baf3d10592b581c7f2843
Dylan Freedman <freedmand@gmail.com>
2020-03-20 16:11:29 +0000

    Fix page specs when empty

---
+++ src/api/pageSize.test.js
@@ -0,0 +1,13 @@
+import { pageSizesFromSpec } from "./pageSize";
+
+test("page sizes empty", () => {
+  expect(pageSizesFromSpec("")).toEqual([]);
+});
+
+test("page sizes simple", () => {
+  expect(pageSizesFromSpec("1x1:0")).toEqual([1]);
+});
+
+test("page sizes complex", () => {
+  expect(pageSizesFromSpec("1x1:1,3-5,0;1x2:2")).toEqual([1, 1, 2, 1, 1, 1]);
+});
