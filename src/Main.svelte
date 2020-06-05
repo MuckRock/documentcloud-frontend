7c373d4d196fcbba2a5099fa78283baf5f626d91
Dylan Freedman <freedmand@gmail.com>
2020-06-05 17:46:19 +0000

    Fix routing

--- src/Main.svelte
+++ src/Main.svelte
@@ -10,6 +10,8 @@
   // Set up routes
   router.routes = new Router(...routes);
 
+  let resolvedRoute = null;
+
   onMount(() => {
     router.currentUrl = currentUrl();
     if (!history.state) {
@@ -19,6 +21,8 @@
         window.location.href
       );
     }
+
+    router.subscribe(() => (resolvedRoute = router.resolvedRoute));
   });
 
   function handleBackNav(e) {
@@ -113,8 +117,6 @@
 
 <svelte:window on:popstate={handleBackNav} />
 
-{#if $router.resolvedRoute != null}
-  <svelte:component
-    this={$router.resolvedRoute.component}
-    {...$router.resolvedRoute.props} />
+{#if resolvedRoute != null}
+  <svelte:component this={resolvedRoute.component} {...resolvedRoute.props} />
 {/if}
