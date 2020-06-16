5187b829287f546d2cdeb01e350f48ed36e9229e
Dylan Freedman <freedmand@gmail.com>
2020-06-16 20:08:27 +0000

    Fix reactivity

--- src/pages/app/App.svelte
+++ src/pages/app/App.svelte
@@ -4,35 +4,23 @@
 
   import { layout } from "@/manager/layout";
   import { documents } from "@/manager/documents";
-  import { onMount, onDestroy } from "svelte";
 
   let sidebar = null;
 
   function setSidebarExpanded(expanded) {
     layout.sidebarExpanded = expanded;
   }
-
-  let reactiveLayout = {};
-  let reactiveDocuments = {};
-  let unsubscriptions = [];
-
-  onMount(() => {
-    unsubscriptions = [
-      layout.subscribe(() => (reactiveLayout = layout || {})),
-      documents.subscribe(() => (reactiveDocuments = documents || {}))
-    ];
-  });
 </script>
 
 <div>
   <Sidebar
     bind:this={sidebar}
     on:retractSidebar={() => setSidebarExpanded(false)}
-    expanded={reactiveLayout.sidebarExpanded} />
+    expanded={$layout.sidebarExpanded} />
   <MainContainer
     on:expandSidebar={() => setSidebarExpanded(true)}
-    concealed={reactiveLayout.sidebarExpanded}
-    documents={reactiveDocuments.documents}
-    loading={reactiveLayout.loading}
-    error={reactiveLayout.error} />
+    concealed={$layout.sidebarExpanded}
+    documents={$documents.documents}
+    loading={$layout.loading}
+    error={$layout.error} />
 </div>
