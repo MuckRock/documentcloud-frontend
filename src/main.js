bca46f970487bcd4ba74fbc8a360515c3e51cf97
Dylan Freedman <freedmand@gmail.com>
2020-12-16 16:06:07 +0000

    Remove all other imports aside from orgs/users

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import Main from './Main.svelte';
 
 // Imports to get persistent app functionality working
 import '@/manager/orgsAndUsers';
-import '@/manager/documents';
-import '@/manager/layout';
-import '@/search/search';
-import '@/viewer/document';
-import '@/viewer/viewer';
-import '@/viewer/layout';
 
 const app = new Main({
   target: document.body,
