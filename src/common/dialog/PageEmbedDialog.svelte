b3312497d58aff801acd72c477b9ff7999e612aa
Dylan Freedman <freedmand@gmail.com>
2021-02-25 17:23:29 +0000

    Small clean-up

--- src/common/dialog/PageEmbedDialog.svelte
+++ src/common/dialog/PageEmbedDialog.svelte
@@ -4,17 +4,8 @@
   import { layout } from "@/viewer/layout";
   import { getEmbed } from "@/api/embed";
   import { doc } from "@/viewer/document";
-  import { pageImageUrl, textUrl } from "@/api/viewer";
 
   let selectedPage = null;
-
-  const IMAGE_WIDTHS = process.env.IMAGE_WIDTHS.split(",")
-    .map((x) => x.split(":"))
-    .map((x) => [parseFloat(x[1]), x[0]])
-    .sort((a, b) => a[0] - b[0]);
-  const LARGE_WIDTH = IMAGE_WIDTHS.map((x, i) => [x, i]).filter(
-    (x) => x[0][1] == "large"
-  )[0];
 
   let page = "visible";
   let anotherNumber = 1;
@@ -57,24 +48,26 @@ Select the page to embed:
   <option value="another">Other page</option>
 </select>
 
-{#if page == 'another'}
+{#if page == "another"}
   <div class="another">
     Enter the page number to embed:
     <BoundedNumberInput
       min={1}
       max={$layout.embedDocument.pageCount}
-      bind:value={anotherNumber} />
+      bind:value={anotherNumber}
+    />
   </div>
 {/if}
 
 <ShareOptions
-  embedDescription={'Copy the HTML code to embed this page within an article or post:'}
+  embedDescription={"Copy the HTML code to embed this page within an article or post:"}
   {embedCode}
   {errorOccurred}
   embedAction={() => {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = enhanceSrc;
     document.body.appendChild(script);
   }}
   linkText={pageUrl}
-  tweetText={`Page ${visiblePage} of ${$layout.embedDocument.title} ${pageUrl}`} />
+  tweetText={`Page ${visiblePage} of ${$layout.embedDocument.title} ${pageUrl}`}
+/>
