3ea468a47d8c65926b853ae469a0dd1d1715a39c
Dylan Freedman <freedmand@gmail.com>
2021-03-12 19:26:22 +0000

    Fix home link style

--- src/pages/home/Home.svelte
+++ src/pages/home/Home.svelte
@@ -3,20 +3,6 @@
   import Link from "@/router/Link";
 </script>
 
-<style lang="scss">
-  a {
-    color: $primary;
-  }
-
-  .note {
-    background: $fyi;
-    border-radius: $radius;
-    padding: 5px 10px;
-    margin: 0 -10px;
-    box-shadow: 0 0 2px rgba(0, 0, 0, 0.12);
-  }
-</style>
-
 <svelte:head>
   <title>DocumentCloud</title>
 </svelte:head>
@@ -24,7 +10,7 @@
 <HomeTemplate showMast="true">
   <p>
     DocumentCloud is an all-in-one platform for documents: upload, organize,
-    analyze, annotate, search, and embed. <Link to="app"
+    analyze, annotate, search, and embed. <Link color={true} to="app"
       >View public documents</Link
     >
   </p>
