7836ba977a0def3fa160e95da23288f4c69c6a48
Dylan Freedman <freedmand@gmail.com>
2021-05-10 17:33:26 +0000

    Autocomplete

--- src/common/Autocomplete.svelte
+++ src/common/Autocomplete.svelte
@@ -1,6 +1,99 @@
 <script>
   export let value;
-  console.log(value);
+  export let method;
+  export let placeholder = "";
+  let inputValue = "";
+  let inputId = 0;
+
+  let completions = null;
+
+  async function autocomplete() {
+    // Use an id to make sure to only report most recent result
+    inputId++;
+    const originalId = inputId;
+    // Apply latency to reduce flow of requests during typing
+    setTimeout(async () => {
+      if (inputId != originalId) return;
+      const results = await method(inputValue);
+      if (inputId == originalId) {
+        completions = results;
+      }
+    }, 100);
+  }
+
+  $: {
+    autocomplete(inputValue);
+  }
 </script>
 
-<input value={value.name} />
+<style lang="scss">
+  .autocomplete {
+    display: inline-block;
+    background: white;
+    border: solid 1px gainsboro;
+    border-radius: $radius;
+
+    .message {
+      font-style: italic;
+      padding: 3px 9px;
+    }
+
+    .tag {
+      display: inline-block;
+      background: whitesmoke;
+      padding: 3px 9px;
+      margin: 3px;
+      border-radius: $radius;
+      box-shadow: 0 0 2px rgba(0, 0, 0, 0.33);
+
+      &.clickable {
+        @include buttonLike;
+        font-style: italic;
+        background: ivory;
+      }
+    }
+
+    input {
+      width: 100%;
+      height: 100%;
+      background: none;
+      border: none;
+      box-sizing: border-box;
+
+      &:disabled {
+        color: black;
+        user-select: none;
+      }
+    }
+  }
+</style>
+
+<span class="autocomplete">
+  {#if value != null}
+    <span class="tag"
+      >{value.name}
+      <span
+        class="close"
+        on:click={() => {
+          value = null;
+          completions = null;
+          inputValue = "";
+          autocomplete();
+        }}>×</span
+      ></span
+    >
+  {:else}
+    <input {placeholder} bind:value={inputValue} />
+    {#if completions == null}
+      <span class="message">Loading completions...</span>
+    {:else if completions.length > 0}
+      {#each completions as completion}
+        <span class="tag clickable" on:click={() => (value = completion)}
+          >{completion.name}</span
+        >
+      {/each}
+    {:else}
+      <span class="message">No results found</span>
+    {/if}
+  {/if}
+</span>
