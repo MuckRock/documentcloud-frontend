4053424ca166ce3fc1bad8c5276f47f67b5a9f8b
Thad Kerosky <contact@thadk.net>
2022-02-28 09:22:22 -0500

    JSON-Schema form from backend

--- src/common/dialog/AddonDispatchDialog.svelte
+++ src/common/dialog/AddonDispatchDialog.svelte
@@ -1,9 +1,9 @@
-<script>
+<script lang="ts">
   import Button from "@/common/Button";
   import Tooltip from "@/common/Tooltip";
   import HtmlEditor from "@/common/HtmlEditor";
   import { addonDialogs } from "./addonDialogs";
-  import {dispatchAddon} from "@/manager/addons"
+  import { addons, dispatchAddon } from "@/manager/addons";
   import { viewer } from "@/viewer/viewer";
   import emitter from "@/emit";
   import { _ } from "svelte-i18n";
@@ -11,6 +11,185 @@
   // Stores
   import { layout } from "@/manager/layout";
   import { layout as viewerLayout } from "@/viewer/layout";
+
+  import Ajv from "ajv";
+  import { Form, components } from "@pyoner/svelte-form";
+  import { createAjvValidator } from "@pyoner/svelte-form-ajv";
+
+  
+  import * as simple from "./samples/simple";
+  import * as single from "./samples/single";
+  import * as array from "./samples/array";
+
+  import jsonSchemaDraft7 from "ajv/lib/refs/json-schema-draft-07.json";
+
+  let samples = [
+    ["Simple", simple],
+    ["Single", single],
+    ["Array", array],
+  ];
+
+  const jsonSchemaDraft4 = {
+    id: "http://json-schema.org/draft-04/schema#",
+    $schema: "http://json-schema.org/draft-04/schema#",
+    description: "Core schema meta-schema",
+    definitions: {
+      schemaArray: {
+        type: "array",
+        minItems: 1,
+        items: { $ref: "#" },
+      },
+      positiveInteger: {
+        type: "integer",
+        minimum: 0,
+      },
+      positiveIntegerDefault0: {
+        allOf: [{ $ref: "#/definitions/positiveInteger" }, { default: 0 }],
+      },
+      simpleTypes: {
+        enum: [
+          "array",
+          "boolean",
+          "integer",
+          "null",
+          "number",
+          "object",
+          "string",
+        ],
+      },
+      stringArray: {
+        type: "array",
+        items: { type: "string" },
+        minItems: 1,
+        uniqueItems: true,
+      },
+    },
+    type: "object",
+    properties: {
+      id: {
+        type: "string",
+      },
+      $schema: {
+        type: "string",
+      },
+      title: {
+        type: "string",
+      },
+      description: {
+        type: "string",
+      },
+      default: {},
+      multipleOf: {
+        type: "number",
+        minimum: 0,
+        exclusiveMinimum: true,
+      },
+      maximum: {
+        type: "number",
+      },
+      exclusiveMaximum: {
+        type: "boolean",
+        default: false,
+      },
+      minimum: {
+        type: "number",
+      },
+      exclusiveMinimum: {
+        type: "boolean",
+        default: false,
+      },
+      maxLength: { $ref: "#/definitions/positiveInteger" },
+      minLength: { $ref: "#/definitions/positiveIntegerDefault0" },
+      pattern: {
+        type: "string",
+        format: "regex",
+      },
+      additionalItems: {
+        anyOf: [{ type: "boolean" }, { $ref: "#" }],
+        default: {},
+      },
+      items: {
+        anyOf: [{ $ref: "#" }, { $ref: "#/definitions/schemaArray" }],
+        default: {},
+      },
+      maxItems: { $ref: "#/definitions/positiveInteger" },
+      minItems: { $ref: "#/definitions/positiveIntegerDefault0" },
+      uniqueItems: {
+        type: "boolean",
+        default: false,
+      },
+      maxProperties: { $ref: "#/definitions/positiveInteger" },
+      minProperties: { $ref: "#/definitions/positiveIntegerDefault0" },
+      required: { $ref: "#/definitions/stringArray" },
+      additionalProperties: {
+        anyOf: [{ type: "boolean" }, { $ref: "#" }],
+        default: {},
+      },
+      definitions: {
+        type: "object",
+        additionalProperties: { $ref: "#" },
+        default: {},
+      },
+      properties: {
+        type: "object",
+        additionalProperties: { $ref: "#" },
+        default: {},
+      },
+      patternProperties: {
+        type: "object",
+        additionalProperties: { $ref: "#" },
+        default: {},
+      },
+      dependencies: {
+        type: "object",
+        additionalProperties: {
+          anyOf: [{ $ref: "#" }, { $ref: "#/definitions/stringArray" }],
+        },
+      },
+      enum: {
+        type: "array",
+        minItems: 1,
+        uniqueItems: true,
+      },
+      type: {
+        anyOf: [
+          { $ref: "#/definitions/simpleTypes" },
+          {
+            type: "array",
+            items: { $ref: "#/definitions/simpleTypes" },
+            minItems: 1,
+            uniqueItems: true,
+          },
+        ],
+      },
+      format: { type: "string" },
+      allOf: { $ref: "#/definitions/schemaArray" },
+      anyOf: { $ref: "#/definitions/schemaArray" },
+      oneOf: { $ref: "#/definitions/schemaArray" },
+      not: { $ref: "#" },
+    },
+    dependencies: {
+      exclusiveMaximum: ["maximum"],
+      exclusiveMinimum: ["minimum"],
+    },
+    default: {},
+  };
+
+  const ajv = new Ajv({
+    schemaId: "auto",
+    jsonPointers: true,
+    allErrors: true,
+    coerceTypes: true,
+  });
+  ajv.addMetaSchema(jsonSchemaDraft4);
+
+  const validator = createAjvValidator(ajv);
+
+  let schema, value;
+
+  schema = layout.addonDispatchOpen.parameters;
+
+  let data = "";
 
   const fieldValid = (value, initial) => value != initial;
   const fieldInvalidText = (value, initial, fieldName) =>
@@ -20,6 +199,7 @@
     dismiss() {},
   });
 
+  $: addonDispatchOpen = JSON.stringify($layout.addonDispatchOpen);
   $: isViewer = $viewer.document != null;
   $: numSelected = isViewer ? 1 : $layout.numSelected;
   $: selected = isViewer ? [$viewer.document] : $layout.selected;
@@ -64,11 +244,7 @@
         fields[addonField.apiField] = values[i];
       }
     }
-    dispatchAddon(
-      parseInt(layout.addonDispatchOpen.id, 10),
-      fields,
-      selected,
-    );
+    dispatchAddon(parseInt(layout.addonDispatchOpen.id, 10), fields, selected);
     emit.dismiss();
   }
 </script>
@@ -100,7 +276,7 @@
         values: { n: numSelected },
       })}
     </h1>
-    <table>
+    <!-- <table>
       {#each addonDialogs as addonField, i}
         {#if addonField.disabled == null || !addonField.disabled(numSelected)}
           <tr>
@@ -113,15 +289,62 @@
                     bind:value={values[i]}
                   />
                 {:else}
-                  <input maxlength={addonField.charLimit} bind:value={values[i]} />
+                  <input
+                    maxlength={addonField.charLimit}
+                    bind:value={values[i]}
+                  />
                 {/if}
               </div>
             </td>
           </tr>
         {/if}
       {/each}
-    </table>
-    <div class="buttonpadded">
+    </table> -->
+    <h5>Other form options:</h5>
+    <ul>
+      {#each samples as [name, module] (name)}
+        <li>
+          <a
+            href="#{name}"
+            on:click={() => {
+              schema = module.schema;
+              value = module.value;
+            }}
+          >
+            {name}
+          </a>
+        </li>
+      {/each}
+    </ul>
+
+    {#if schema}
+      <h1>Form</h1>
+      <Form {schema} {components} {value} {validator}
+        on:submit={(e) => {
+          console.log("submits", e);
+          data = JSON.stringify(e.detail);
+          dispatchAddon(
+            parseInt(layout.addonDispatchOpen.id, 10),
+            data,
+            selected,
+          );
+          emit.dismiss();
+        }}
+        on:reset={(e) => {
+          console.log("reset", e);
+        }}
+        >
+        <div class="buttonpadded">
+          <button type="reset">Reset</button>
+          <Button type="submit">{$_("dialog.dispatch")}</Button>
+          <Button secondary={true} on:click={emit.dismiss}
+            >{$_("dialog.cancel")}</Button
+          >
+        </div>
+      </Form>
+
+    {/if}
+    <!-- <div class="buttonpadded">
       {#if valid}
         <Button on:click={applyAction}>{$_("dialog.dispatch")}</Button>
       {:else}
@@ -132,6 +355,6 @@
       <Button secondary={true} on:click={emit.dismiss}
         >{$_("dialog.cancel")}</Button
       >
-    </div>
+    </div> -->
   </div>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -3,4 +3,12 @@ const { preprocessOptions } = require("./preprocess.config.js");
 
 module.exports = {
   preprocess: autoPreprocess(preprocessOptions),
+  onwarn: (warning, handler) => { /* see also ignoreWarning in base webpack https://github.com/sveltejs/svelte-loader/pull/96#issuecomment-887691760 */
+    const { code, frame } = warning;
+    if (code === "css-unused-selector")
+        return;
+
+    handler(warning);
+}
+/* look in webpack.base.config.js for this functionality */
 };
