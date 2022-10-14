10f36a7ca6aea23c863b7264c041e03a48cde4aa
Mitchell Kotler <mitch@mitchellkotler.net>
2022-10-14 13:37:46 -0400

    only show valid ocr engine options

--- src/common/UploadOptions.svelte
+++ src/common/UploadOptions.svelte
@@ -1,14 +1,22 @@
 <script>
   import Omniselect from "./Omniselect";
   import Checkbox from "./Checkbox";
-  import { defaultLanguage, languages } from "@/api/languages";
+  import { defaultLanguage, languages, textractLanguages } from "@/api/languages";
+  import { orgsAndUsers } from "@/manager/orgsAndUsers";
   import { _ } from "svelte-i18n";
 
   export let language = defaultLanguage;
   export let forceOcr = false;
   export let ocrEngine = "tess4";
 
-  const ocrEngines = [["tess4", "Tesseract"], ["textract", "Textract"]];
+  const showOcrEngine = orgsAndUsers.me.feature_level > 0;
+
+  $: selectLanguages = ocrEngine == "textract" ? textractLanguages : languages;
+
+  const ocrEngines = [
+    ["tess4", "Tesseract"],
+    ["textract", "Textract"],
+  ];
 </script>
 
 <style lang="scss">
@@ -21,7 +29,7 @@
 
 <div class="option">
   <b>{$_("uploadOptions.documentLang")}</b>
-  <Omniselect options={languages} bind:selected={language} />
+  <Omniselect options={selectLanguages} bind:selected={language} />
 </div>
 
 <div class="option">
@@ -31,8 +39,9 @@
   </div>
 </div>
 
-<div class="option">
-  <b>{$_("uploadOptions.ocrEngine")}</b>
-  <Omniselect options={ocrEngines} bind:selected={ocrEngine} />
-</div>
-
+{#if showOcrEngine}
+  <div class="option">
+    <b>{$_("uploadOptions.ocrEngine")}</b>
+    <Omniselect options={ocrEngines} bind:selected={ocrEngine} />
+  </div>
+{/if}
