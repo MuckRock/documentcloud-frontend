672cae13ae9f0918b35838b3adefd637bc080a61
Mitchell Kotler <mitch@mitchellkotler.net>
2022-10-26 15:14:08 -0400

    show the user their remaining ai credits

--- src/common/UploadOptions.svelte
+++ src/common/UploadOptions.svelte
@@ -1,15 +1,20 @@
 <script>
   import Omniselect from "./Omniselect";
   import Checkbox from "./Checkbox";
-  import { defaultLanguage, languages, textractLanguages } from "@/api/languages";
-  import { orgsAndUsers } from "@/manager/orgsAndUsers";
+  import {
+    defaultLanguage,
+    languages,
+    textractLanguages,
+  } from "@/api/languages";
+  import { orgsAndUsers, initOrgsAndUsers } from "@/manager/orgsAndUsers";
+  import { onMount } from "svelte";
   import { _ } from "svelte-i18n";
 
   export let language = defaultLanguage;
   export let forceOcr = false;
   export let ocrEngine = "tess4";
 
-  const showOcrEngine = orgsAndUsers.me.feature_level > 0;
+  const showOcrEngine = $orgsAndUsers.me.feature_level > 0;
 
   $: selectLanguages = ocrEngine == "textract" ? textractLanguages : languages;
 
@@ -17,6 +22,10 @@
     ["tess4", "Tesseract"],
     ["textract", "Textract"],
   ];
+
+  onMount(async () => {
+    await initOrgsAndUsers();
+  });
 </script>
 
 <style lang="scss">
@@ -43,5 +52,13 @@
   <div class="option">
     <b>{$_("uploadOptions.ocrEngine")}</b>
     <Omniselect options={ocrEngines} bind:selected={ocrEngine} />
+    <div class="small">
+      {$_("uploadOptions.creditHelpText", {
+        values: {
+          organization: $orgsAndUsers.me.organization.name,
+          n: $orgsAndUsers.me.organization.monthly_ai_credits,
+        },
+      })}
+    </div>
   </div>
 {/if}
