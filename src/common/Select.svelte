ec013e7231f74fa5c1aafb94075a331aba202275
Chris Amico <eyeseast@gmail.com>
2023-03-29 15:14:10 -0400

    Better default language selection

---
+++ src/common/Select.svelte
@@ -0,0 +1,54 @@
+<script>
+  export let name;
+  export let label;
+  export let options = []; // [value, name, disabled]
+  export let selected;
+  export let value = null;
+
+  let input;
+
+  $: valueToName = options.reduce((m, opt) => {
+    m.set(opt[0], opt[1]);
+    return m;
+  }, new Map());
+
+  $: nameToValue = options.reduce((m, opt) => {
+    m.set(opt[1], opt[0]);
+    return m;
+  }, new Map());
+
+  $: value = nameToValue.get(selected);
+
+  function onFocus(e) {
+    selected = input.value;
+    input.value = "";
+  }
+
+  function onBlur(e) {
+    input.value = selected;
+  }
+</script>
+
+<label>
+  {label}
+  <input
+    type="search"
+    {name}
+    list={name}
+    bind:this={input}
+    bind:value={selected}
+    on:focus={onFocus}
+    on:blur={onBlur}
+  />
+  <datalist id={name}>
+    {#if nameToValue.has(selected)}
+      <optgroup>
+        <option>{selected}</option>
+      </optgroup>
+    {/if}
+
+    {#each options as [value, name, disabled]}
+      <option {disabled}>{name}</option>
+    {/each}
+  </datalist>
+</label>
