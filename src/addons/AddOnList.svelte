0b73f1a06ea52809b8dfe0b1d05a9e77ff46e88d
Allan Lasser <lasser.allan@gmail.com>
2023-05-26 16:39:49 -0400

    Creates AddOnList component

---
+++ src/addons/AddOnList.svelte
@@ -0,0 +1,48 @@
+<script lang="ts" context="module">
+  import type { AddOnListItem } from './AddOnListItem.svelte';
+
+  export interface AddOnList {
+    data?: AddOnListItem[];
+    loading: boolean;
+    error?: string | null;
+  }
+</script>
+
+<script lang="ts">
+  import ListItem from './AddOnListItem.svelte';
+  
+  export let data: AddOnListItem[] = [];
+  export let loading: boolean = false;
+  export let error: string | null = null;
+</script>
+
+<style>
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  li {
+    margin: 1.5rem;
+  }
+</style>
+
+<div class="list">
+  {#if data && data.length > 0}
+  <ul>
+    {#each data as addOn (addOn.id)}
+      <li><ListItem {...addOn} /></li>
+    {/each}
+  </ul>
+  {:else if loading}
+    <!-- Loading state -->
+    <p class="loading">Loading…</p>
+  {:else if error}
+    <!-- Error state -->
+    <p class="error">{error}</p>
+  {:else}
+    <!-- Empty state -->
+    <p class="empty">No results</p>
+  {/if}
+</div>
--- src/addons/AddOnListItem.svelte
+++ src/addons/AddOnListItem.svelte
@@ -1,36 +1,49 @@
-<script lang="ts">
-  import Pin from "../common/Pin.svelte";
-  import Title from "../common/Title.svelte";
-  import { nFormatter } from "../util/string";
-  import Usage from "../assets/usage.svg";
-  import AddOnPopularity from "./AddOnPopularity.svelte";
-
+<script lang="ts" context="module">
   interface Author {
     name?: string;
     avatar?: string;
   }
+  export interface AddOnListItem {
+    pinned: boolean;
+    id: string;
+    title: string;
+    description?: string;
+    author?: Author;
+    usage?: number;
+  }
+</script>
 
-  export let pinned = true;
+<script lang="ts">
+  import Pin from "../common/Pin.svelte";
+  import Title from "../common/Title.svelte";
+  import AddOnPopularity from "./AddOnPopularity.svelte";
+  
+  export let id: string = undefined;
+  export let pinned = false;
   export let title: string = undefined;
   export let description: string = undefined;
-  export let author = {
+  export let author: Author = {
     name: undefined,
     avatar: undefined
   };
   export let usage: number = undefined;
+
+  $: handlePinClick = () => {
+    pinned = !pinned;
+    console.log(`${pinned ? 'Pinning' : 'Unpinning'} ${id}…`);
+  }
 </script>
 
 <style>
   .container {
     display: block;
-    list-style-type: none;
     min-width: 12rem;
   }
 
   .top-row {
     display: flex;
     align-items: flex-end;
-    gap: 1rem;
+    gap: .5rem;
     margin: .5rem;
   }
 
@@ -42,7 +55,9 @@
 
   .description {
     margin: .5rem;
-    opacity: .7;
+    opacity: .6z;
+    font-size: .875em;
+    line-height: 1.4;
   }
 
   .center-self {
@@ -58,9 +73,9 @@
   }
 </style>
 
-<li class="container">
+<div class="container" id={id}>
   <div class="top-row">
-    <div class="center-self"><Pin active={pinned} /></div>
+    <div class="center-self"><Pin active={pinned} on:click={handlePinClick} /></div>
     <div class="stretch"><Title>{title}</Title></div>
     <div class="metadata">
       {#if author}
@@ -74,4 +89,4 @@
   {#if description}
   <p class="description">{description}</p>
   {/if}
-</li>
+</div>
