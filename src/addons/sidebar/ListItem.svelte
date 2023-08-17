32a2b17875bf19859871d1535b1d797748beee62
Chris Amico <eyeseast@gmail.com>
2023-08-17 13:43:08 -0400

    Use icon instead of full pin component

--- src/addons/sidebar/ListItem.svelte
+++ src/addons/sidebar/ListItem.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { AddOnListItem } from "../browser/AddOnListItem.svelte";
-  import Pin from "../../common/Pin.svelte";
+  import Pin from "../../common/icons/Pin.svelte";
 
   export let addon: AddOnListItem;
 
@@ -17,11 +17,18 @@
   h4 {
     margin: 0em auto 1em 0.5em;
   }
+
+  .addon a {
+    fill: var(--highlight-orange);
+  }
 </style>
 
 <div class="addon">
-  <a href={url}><Pin active={addon.active} /></a>
+  <a href={url}>
+    <Pin title={addon.name} />
+  </a>
+
   <h4>
-    <a href={url}>{addon.name}</a>
+    <a href={url}> {addon.name}</a>
   </h4>
 </div>
