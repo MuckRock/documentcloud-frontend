30b8fee8fa010d15d701e4be622b9b87c18b1205
Chris Amico <eyeseast@gmail.com>
2023-08-30 11:04:23 -0400

    fix content width

--- src/pages/FlatPage.svelte
+++ src/pages/FlatPage.svelte
@@ -174,163 +174,165 @@
     margin: 0 auto;
     box-sizing: border-box;
     padding: 40px 20px;
+  }
+
+  .page :global(a) {
+    color: var(--primary, #4294f0);
+  }
+
+  .page .toccontainer {
+    position: absolute;
+    left: 100%;
+    width: var(--tocWidth);
+    padding: 40px var(--tocPaddingRight) 40px var(--tocPaddingLeft);
+    top: 0;
+    bottom: 0;
+  }
+
+  @media only screen and (max-width: 720px) {
+    .page .toccontainer {
+      position: relative;
+      left: inherit;
+      width: inherit;
+      padding: 0;
+      padding-right: 10px;
+    }
+  }
+
+  .page .toccontainer .toc {
+    font-size: 14px;
+    position: sticky;
+    top: 20px;
+    overflow: auto;
+    height: calc(100vh - 40px);
+
+    @media only screen and (max-width: 720px) {
+      position: relative;
+      height: inherit;
+      top: 0;
+      overflow: none;
+
+      background: rgba(var(--primary, #4294f0), 0.05);
+      border: solid 1px var(--primary, #4294f0);
+      padding: 0px 14px;
+      box-sizing: border-box;
+      border-radius: 3px;
+    }
+
+    :global(ul) {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+    }
+
+    :global(ul) :global(ul) {
+      padding-inline-start: 1rem;
+    }
+
+    :global(li) {
+      @media only screen and (max-width: 720px) {
+        :global(.deep) {
+          display: none;
+        }
+      }
+
+      list-style: none;
+      margin: 0.8em 0;
+    }
+  }
+
+  .page .content {
+    max-width: calc(100% - 240px);
+    padding: 20px 0;
+    margin: 20px 0;
+    border-top: solid 1px gainsboro;
+    font-size: 16px;
+    position: relative;
+
+    @media only screen and (max-width: 720px) {
+      max-width: 100%;
+    }
+
+    // Inspired by https://github.com/alex-shpak/hugo-book
+
+    :global(h1),
+    :global(h2),
+    :global(h3),
+    :global(h4),
+    :global(h5),
+    :global(h6) {
+      font-weight: normal;
+
+      :global(.hiddenanchor) {
+        visibility: hidden;
+      }
+
+      &:hover {
+        :global(.hiddenanchor) {
+          visibility: visible;
+        }
+      }
+    }
+
+    :global(h5),
+    :global(h6) {
+      font-weight: bold;
+    }
+
+    :global(h1) {
+      font-size: 32px;
+    }
+
+    :global(h2) {
+      font-size: 24px;
+    }
+
+    :global(h3) {
+      font-size: 20px;
+    }
+
+    :global(h4) {
+      font-size: 18px;
+    }
+
+    :global(code) {
+      padding: 0 0.25rem;
+      background: #e9ecef;
+      border-radius: 0.25rem;
+      font-size: 0.925em;
+    }
 
     :global(a) {
       color: var(--primary, #4294f0);
     }
 
-    .toccontainer {
-      position: absolute;
-      left: 100%;
-      width: var(--tocWidth);
-      padding: 40px var(--tocPaddingRight) 40px var(--tocPaddingLeft);
-      top: 0;
-      bottom: 0;
+    :global(pre) {
+      padding: 1rem;
+      background: #f8f9fa;
+      border-radius: 0.25rem;
+      overflow-x: auto;
 
-      @media only screen and (max-width: 720px) {
-        position: relative;
-        left: inherit;
-        width: inherit;
+      :global(code) {
+        background: none;
         padding: 0;
-        padding-right: 10px;
-      }
-
-      .toc {
-        font-size: 14px;
-        position: sticky;
-        top: 20px;
-        overflow: auto;
-        height: calc(100vh - 40px);
-
-        @media only screen and (max-width: 720px) {
-          position: relative;
-          height: inherit;
-          top: 0;
-          overflow: none;
-
-          background: rgba(var(--primary, #4294f0), 0.05);
-          border: solid 1px var(--primary, #4294f0);
-          padding: 0px 14px;
-          box-sizing: border-box;
-          border-radius: 3px;
-        }
-
-        :global(ul) {
-          padding: 0;
-          margin: 0;
-          list-style: none;
-
-          :global(ul) {
-            padding-inline-start: 1rem;
-          }
-        }
-
-        :global(li) {
-          @media only screen and (max-width: 720px) {
-            :global(.deep) {
-              display: none;
-            }
-          }
-
-          list-style: none;
-          margin: 0.8em 0;
-        }
       }
     }
 
-    .content {
-      max-width: calc(100% - var(--tocFinalWidth));
-      padding: 20px 0;
-      margin: 20px 0;
-      border-top: solid 1px gainsboro;
-      font-size: 16px;
-      position: relative;
+    :global(table) {
+      border-spacing: 0;
+      border-collapse: collapse;
+    }
 
-      @media only screen and (max-width: 720px) {
-        max-width: 100%;
-      }
+    :global(td),
+    :global(th) {
+      border: solid 1px #d1d6dc;
+      padding: 6px 12px;
+      vertical-align: middle;
+    }
 
-      // Inspired by https://github.com/alex-shpak/hugo-book
-
-      :global(h1),
-      :global(h2),
-      :global(h3),
-      :global(h4),
-      :global(h5),
-      :global(h6) {
-        font-weight: normal;
-
-        :global(.hiddenanchor) {
-          visibility: hidden;
-        }
-
-        &:hover {
-          :global(.hiddenanchor) {
-            visibility: visible;
-          }
-        }
-      }
-
-      :global(h5),
-      :global(h6) {
-        font-weight: bold;
-      }
-
-      :global(h1) {
-        font-size: 32px;
-      }
-
-      :global(h2) {
-        font-size: 24px;
-      }
-
-      :global(h3) {
-        font-size: 20px;
-      }
-
-      :global(h4) {
-        font-size: 18px;
-      }
-
-      :global(code) {
-        padding: 0 0.25rem;
-        background: #e9ecef;
-        border-radius: 0.25rem;
-        font-size: 0.925em;
-      }
-
-      :global(a) {
-        color: var(--primary, #4294f0);
-      }
-
-      :global(pre) {
-        padding: 1rem;
-        background: #f8f9fa;
-        border-radius: 0.25rem;
-        overflow-x: auto;
-
-        :global(code) {
-          background: none;
-          padding: 0;
-        }
-      }
-
-      :global(table) {
-        border-spacing: 0;
-        border-collapse: collapse;
-      }
-
-      :global(td),
-      :global(th) {
-        border: solid 1px #d1d6dc;
-        padding: 6px 12px;
-        vertical-align: middle;
-      }
-
-      :global(summary) {
-        outline: none;
-        cursor: pointer;
-      }
+    :global(summary) {
+      outline: none;
+      cursor: pointer;
     }
   }
 </style>
