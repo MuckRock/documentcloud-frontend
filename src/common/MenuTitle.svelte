0600b6b53308229cf2929f267b7f43987ab16569
Allan Lasser <lasser.allan@gmail.com>
2023-10-13 17:02:43 -0400

    Creates secondary account navigation options

---
+++ src/common/MenuTitle.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let label: string;
+</script>
+
+<style>
+  .title {
+    display: flex;
+    align-items: center;
+  }
+  .label {
+    pointer-events: none;
+  }
+  .icon {
+    background-color: rgba(0, 0, 0, 0.1);
+    border-radius: 50%;
+    margin-right: 0.25em;
+  }
+  .dropper {
+    margin-bottom: -0.25em;
+    color: rgba(0, 0, 0, 0.5);
+  }
+</style>
+
+<span class="title">
+  {#if $$slots.icon}<span class="icon"><slot name="icon" /></span>{/if}
+  <span class="label">{label}</span>
+  <span class="dropper">▼</span>
+</span>
---
+++ src/pages/app/AccountNavigation/stories/AccountNavigation.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+
+  import AccountNavigation from "../AccountNavigation.svelte";
+</script>
+
+<Meta
+  title="Account Navigation"
+  component={AccountNavigation}
+  parameters={{ layout: "fullscreen" }}
+/>
+
+<Template let:args>
+  <AccountNavigation {...args} />
+</Template>
+
+<Story name="Default" args={{}} />
