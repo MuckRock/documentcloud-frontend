a55469461c98bcbfa8273723d5d2d52a97c0eb2d
Allan Lasser <lasser.allan@gmail.com>
2023-10-17 12:49:35 -0400

    Adds icons and fallback states to UserMenu

--- src/pages/app/AccountNavigation/UserMenu.svelte
+++ src/pages/app/AccountNavigation/UserMenu.svelte
@@ -1,10 +1,11 @@
 <script lang="ts" context="module">
-  export interface User {
-    name: string;
-    avatar_url: string;
-  }
+  type Maybe<T> = T | undefined | null;
 
-  type Maybe<T> = T | undefined;
+  export interface User {
+    name: Maybe<string>;
+    avatar_url: Maybe<string>;
+    username: string;
+  }
 </script>
 
 <script lang="ts">
@@ -14,6 +15,13 @@
   import Menu from "../../../common/Menu.svelte";
   import MenuItem from "../../../common/MenuItem.svelte";
   import MenuTitle from "../../../common/MenuTitle.svelte";
+
+  import {
+    Paperclip16,
+    Gear16,
+    SignOut16,
+    PersonFill16,
+  } from "svelte-octicons";
 
   import {
     SQUARELET_URL,
@@ -26,28 +34,44 @@
 </script>
 
 <style>
-  .userIcon {
+  .userAvatar {
     height: 1.5rem;
     width: 1.5rem;
     display: block;
+  }
+  .userIcon {
+    height: 1.5rem;
+    width: 1.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
 
 {#if user}
   <Dropdown name="user" fixed={true}>
-    <MenuTitle slot="title" label={user.name}>
-      <img src={user.avatar_url} class="userIcon" alt="" slot="icon" />
+    <MenuTitle slot="title" label={user.name ?? user.username}>
+      <span slot="icon">
+        {#if user.avatar_url}
+          <img src={user.avatar_url} class="userAvatar" alt="" />
+        {:else}
+          <div class="userIcon"><PersonFill16 /></div>
+        {/if}
+      </span>
     </MenuTitle>
     <Menu>
-      <a href={SQUARELET_URL} target="_blank" style="color: inherit">
-        <MenuItem>{$_("authSection.acctSettings")}</MenuItem>
-      </a>
+      <MenuItem href={SQUARELET_URL} target="_blank">
+        <Gear16 slot="icon" />
+        {$_("authSection.acctSettings")}
+      </MenuItem>
       <MenuItem on:click={showMailkeySelected}>
+        <Paperclip16 slot="icon" />
         {$_("authSection.uploadEmail")}
       </MenuItem>
-      <a href={SIGN_OUT_URL} style="color: inherit">
-        <MenuItem>{$_("authSection.signOut")}</MenuItem>
-      </a>
+      <MenuItem href={SIGN_OUT_URL}>
+        <SignOut16 slot="icon" />
+        {$_("authSection.signOut")}
+      </MenuItem>
     </Menu>
   </Dropdown>
 {:else}
--- src/pages/app/AccountNavigation/stories/UserMenu.stories.svelte
+++ src/pages/app/AccountNavigation/stories/UserMenu.stories.svelte
@@ -42,3 +42,55 @@
     },
   }}
 />
+<Story
+  name="Missing Avatar"
+  args={{
+    user: {
+      id: 110237,
+      avatar_url: null,
+      name: "Allan Lasser",
+      organization: {
+        id: 40742,
+        avatar_url:
+          "https://cdn.muckrock.com/media/account_images/allan-headshot-2016.jpg",
+        individual: true,
+        monthly_ai_credits: 2000,
+        name: "lasser.allan",
+        number_ai_credits: 0,
+        slug: "lasserallan",
+        uuid: "9ae6ba3f-d5d4-456c-a7b7-0e83512dfd98",
+      },
+      organizations: [40742, 125],
+      admin_organizations: [40742],
+      username: "lasser.allan",
+      uuid: "9ae6ba3f-d5d4-456c-a7b7-0e83512dfd98",
+      verified_journalist: true,
+    },
+  }}
+/>
+<Story
+  name="Missing Name"
+  args={{
+    user: {
+      id: 110237,
+      avatar_url: null,
+      name: null,
+      organization: {
+        id: 40742,
+        avatar_url:
+          "https://cdn.muckrock.com/media/account_images/allan-headshot-2016.jpg",
+        individual: true,
+        monthly_ai_credits: 2000,
+        name: "lasser.allan",
+        number_ai_credits: 0,
+        slug: "lasserallan",
+        uuid: "9ae6ba3f-d5d4-456c-a7b7-0e83512dfd98",
+      },
+      organizations: [40742, 125],
+      admin_organizations: [40742],
+      username: "lasser.allan",
+      uuid: "9ae6ba3f-d5d4-456c-a7b7-0e83512dfd98",
+      verified_journalist: true,
+    },
+  }}
+/>
