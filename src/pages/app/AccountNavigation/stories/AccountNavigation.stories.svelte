d33d04eeaf97763a7c87c46714966d9e1cb1cd6d
Allan Lasser <lasser.allan@gmail.com>
2023-10-25 14:49:15 -0400

    Mocks data loading in stories

--- src/pages/app/AccountNavigation/stories/AccountNavigation.stories.svelte
+++ src/pages/app/AccountNavigation/stories/AccountNavigation.stories.svelte
@@ -1,4 +1,29 @@
-<script>
+<script context="module" lang="ts">
+  import { rest } from "msw";
+  import { baseApiUrl } from "../../../../api/base.js";
+  import { mockInMyOrg } from "./OrgMemberList.stories.svelte";
+  import {
+    mockGetOrgsList,
+    mockChangeOrg,
+    mockGetOrg,
+  } from "./OrgMenu.stories.svelte";
+  import getMeFixture from "../fixtures/getMe.json";
+
+  /* Mock Request Handlers */
+  const mockUrl = new URL(`users/me/`, baseApiUrl).toString();
+  export const mockGetMe = {
+    data: rest.get(mockUrl, (req, res, ctx) => res(ctx.json(getMeFixture))),
+    noOrgs: rest.get(mockUrl, (req, res, ctx) =>
+      res(ctx.json({ ...getMeFixture, organization: "4" })),
+    ),
+    loading: rest.get(mockUrl, (req, res, ctx) => res(ctx.delay("infinite"))),
+    error: rest.get(mockUrl, (req, res, ctx) =>
+      res(ctx.status(404, "Not Found"), ctx.json({ detail: "Not found." })),
+    ),
+  };
+</script>
+
+<script lang="ts">
   import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
 
   import AccountNavigation from "../AccountNavigation.svelte";
@@ -14,4 +39,41 @@
   <AccountNavigation {...args} />
 </Template>
 
-<Story name="Default" args={{}} />
+<Story
+  name="Success"
+  parameters={{
+    msw: {
+      handlers: [
+        mockGetMe.data,
+        mockGetOrgsList.data,
+        mockInMyOrg.data,
+        mockChangeOrg,
+        mockGetOrg.data,
+      ],
+    },
+  }}
+/>
+<Story
+  name="Loading"
+  parameters={{
+    msw: {
+      handlers: [
+        mockGetMe.loading,
+        mockGetOrgsList.loading,
+        mockInMyOrg.loading,
+      ],
+    },
+  }}
+/>
+<Story
+  name="Signed Out"
+  parameters={{ msw: { handlers: [mockGetMe.error] } }}
+/>
+<Story
+  name="No Orgs"
+  parameters={{
+    msw: {
+      handlers: [mockGetMe.noOrgs, mockGetOrg.data, mockGetOrgsList.empty],
+    },
+  }}
+/>
--- src/pages/app/AccountNavigation/stories/OrgMenu.stories.svelte
+++ src/pages/app/AccountNavigation/stories/OrgMenu.stories.svelte
@@ -3,26 +3,77 @@
   import { baseApiUrl } from "../../../../api/base.js";
   import { mockInMyOrg } from "./OrgMemberList.stories.svelte";
   import orgListFixture from "../fixtures/orgList.json";
+  import getMeFixture from "../fixtures/orgList.json";
 
   /* Mock Request Handlers */
-  const mockUrl = new URL(`organizations/`, baseApiUrl).toString();
+  const mockGetOrgsUrl = new URL(`organizations/`, baseApiUrl).toString();
   export const mockGetOrgsList = {
-    data: rest.get(mockUrl, (req, res, ctx) => res(ctx.json(orgListFixture))),
-    loading: rest.get(mockUrl, (req, res, ctx) => res(ctx.delay("infinite"))),
-    error: rest.get(mockUrl, (req, res, ctx) =>
+    data: rest.get(mockGetOrgsUrl, (req, res, ctx) =>
+      res(ctx.json(orgListFixture)),
+    ),
+    loading: rest.get(mockGetOrgsUrl, (req, res, ctx) =>
+      res(ctx.delay("infinite")),
+    ),
+    error: rest.get(mockGetOrgsUrl, (req, res, ctx) =>
       res(
         ctx.status(400, "Ambiguous Error"),
         ctx.json("Something went horribly wrong."),
       ),
     ),
-    empty: rest.get(mockUrl, (req, res, ctx) =>
+    empty: rest.get(mockGetOrgsUrl, (req, res, ctx) =>
+      res(
+        ctx.json({
+          next: null,
+          previous: null,
+          results: [
+            {
+              id: 4,
+              avatar_url:
+                "https://cdn.muckrock.com/media/account_images/allan-headshot-2016.jpg",
+              individual: true,
+              monthly_ai_credits: 2000,
+              number_ai_credits: 0,
+              name: "lasser.allan",
+              slug: "lasserallan",
+            },
+          ],
+        }),
+      ),
+    ),
+  };
+
+  const mockGetOrgUrl = new URL(`organizations/:id/*`, baseApiUrl).toString();
+  export const mockGetOrg = {
+    data: rest.get(mockGetOrgUrl, (req, res, ctx) =>
+      res(
+        ctx.json(
+          orgListFixture.results.find(
+            ({ id }) => id.toString() === req.params.id,
+          ),
+        ),
+      ),
+    ),
+    loading: rest.get(mockGetOrgUrl, (req, res, ctx) =>
+      res(ctx.delay("infinite")),
+    ),
+    error: rest.get(mockGetOrgUrl, (req, res, ctx) =>
+      res(
+        ctx.status(400, "Ambiguous Error"),
+        ctx.json("Something went horribly wrong."),
+      ),
+    ),
+    empty: rest.get(mockGetOrgUrl, (req, res, ctx) =>
       res(ctx.json({ next: null, previous: null, results: [] })),
     ),
   };
+
+  const mockChangeOrgUrl = new URL(`users/me/`, baseApiUrl).toString();
+  export const mockChangeOrg = rest.patch(mockChangeOrgUrl, (req, res, ctx) =>
+    res(ctx.json(getMeFixture)),
+  );
 </script>
 
 <script>
-  import { action } from "@storybook/addon-actions";
   import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
 
   import OrgMenu from "../OrgMenu.svelte";
@@ -31,7 +82,7 @@
     user: {
       id: 4,
     },
-    activeOrg: {
+    org: {
       id: 1,
       avatar_url: "https://cdn.muckrock.com/media/org_avatars/logo.png",
       individual: false,
@@ -40,8 +91,6 @@
       name: "MuckRock",
       slug: "muckrock",
     },
-    userOrgs: [1, 2],
-    changeOrg: action("Change Org"),
   };
 </script>
 
@@ -58,17 +107,42 @@
 <Story
   name="Success"
   {args}
-  parameters={{ msw: { handlers: [mockInMyOrg.data, mockGetOrgsList.data] } }}
+  parameters={{
+    msw: {
+      handlers: [
+        mockInMyOrg.data,
+        mockGetOrgsList.data,
+        mockGetOrg.data,
+        mockChangeOrg,
+      ],
+    },
+  }}
 />
 <Story
   name="Loading"
   {args}
   parameters={{
-    msw: { handlers: [mockInMyOrg.loading, mockGetOrgsList.loading] },
+    msw: {
+      handlers: [
+        mockInMyOrg.loading,
+        mockGetOrgsList.loading,
+        mockGetOrg.loading,
+        mockChangeOrg,
+      ],
+    },
   }}
 />
 <Story
   name="Without Avatar"
-  args={{ ...args, activeOrg: { ...args.activeOrg, avatar_url: "" } }}
-  parameters={{ msw: { handlers: [mockInMyOrg.data, mockGetOrgsList.data] } }}
+  args={{ ...args, org: { ...args.org, avatar_url: "" } }}
+  parameters={{
+    msw: {
+      handlers: [
+        mockInMyOrg.data,
+        mockGetOrgsList.data,
+        mockGetOrg.data,
+        mockChangeOrg,
+      ],
+    },
+  }}
 />
