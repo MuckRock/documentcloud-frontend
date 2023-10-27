c678858f4fee6defeee26fe7fe8a7298ae3359b0
Mitchell Kotler <mitch@mitchellkotler.net>
2023-10-27 15:23:05 -0400

    fi xpublic text files

--- src/api/session.js
+++ src/api/session.js
@@ -87,15 +87,28 @@ session.getStatic = async function getStatic(url) {
   if (sessionCache.has(url)) {
     return sessionCache.lookup(url);
   }
+  let result;
 
-  let result = await session.get(url).then((r) => r.data);
-  let redirect = result.location;
+  // If we are fetching a private asset, we authenticate through our own app server
+  // We check this by checking if the URL starts with DC_BASE
+  // We must send the session cookie with the first request, which should redirect
+  // to a S3 link.  S3 will error out if sent the session cookie, so we must send
+  // the location in the data of a 200 response instead of a normal 302 response.
+  // On the second request, we do not sent the session cookie
+  // If we are fetching a public asset, the first request is directly to S3.  In
+  // that case we must not send the session cookie on the first request.
+  if (url.startsWith(process.env.DC_BASE)) {
+    result = await session.get(url).then((r) => r.data);
+    let redirect = result.location;
 
-  // text requests return a 200 but have a location property in the JSON result
-  // this lets us send a second request to S3 without credentials
-  if (redirect) {
-    // note that this uses plain axios, not the session
-    result = await axios.get(redirect).then((r) => r.data);
+    // text requests return a 200 but have a location property in the JSON result
+    // this lets us send a second request to S3 without credentials
+    if (redirect) {
+      // note that this uses plain axios, not the session
+      result = await axios.get(redirect).then((r) => r.data);
+    }
+  } else {
+      result = await axios.get(url).then((r) => r.data);
   }
 
   sessionCache.cache(url, result);
