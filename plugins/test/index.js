8d3751adba6ca7fb671ae0406193f4dc5667e84b
Chris Amico <eyeseast@gmail.com>
2023-11-14 16:10:14 -0500

    deps

--- plugins/test/index.js
+++ plugins/test/index.js
@@ -1,6 +1,6 @@
 // netlify plugin to run playwright on deploy previews
 
 export async function onSuccess({ utils }) {
-  await utils.run("playwright", ["install"]);
+  await utils.run("playwright", ["install", "--with-deps"]);
   await utils.run("playwright", ["test"]);
 }
