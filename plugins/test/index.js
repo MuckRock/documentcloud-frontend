7a85fe5f0e7ba797e6951c3f091a097a5d31bfb9
Chris Amico <eyeseast@gmail.com>
2023-11-14 16:58:44 -0500

    logging

--- plugins/test/index.js
+++ plugins/test/index.js
@@ -1,7 +1,12 @@
 // netlify plugin to run playwright on deploy previews
 
 export async function onSuccess({ utils }) {
+  console.log("Installing Playwright dependencies");
   await utils.run("playwright", ["install-deps"]);
   await utils.run("playwright", ["install"]);
+
+  console.log("Running Playwright tests");
   await utils.run("playwright", ["test"]);
+
+  console.log("Done.");
 }
