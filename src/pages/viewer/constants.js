f947aa27516d47d96d188be73a9ce25aae0ffb47
Allan Lasser <lasser.allan@gmail.com>
2023-11-22 17:41:31 -0500

    Fixes height constants

--- src/pages/viewer/constants.js
+++ src/pages/viewer/constants.js
@@ -1,5 +1,5 @@
-export const HEADER_HEIGHT = 32;
-export const FOOTER_HEIGHT = 32;
+export const HEADER_HEIGHT = 36;
+export const FOOTER_HEIGHT = 36;
 export const SIDEBAR_WIDTH = 350;
 // A little bigger than normal mobile break to hide sidebar in narrow viewports
 export const MOBILE_BREAKPOINT = 800;
