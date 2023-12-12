0bdb3e103f301073c0bcd006ada9f47a2dd7d09f
Chris Amico <eyeseast@gmail.com>
2023-12-12 12:16:30 -0500

    Delete unused files

--- browser-test-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-BROWSER=firefox node tests/functional/suites/noindex.js
-#BROWSER=chromium node tests/functional/suites/noindex.js
-BROWSER=webkit node tests/functional/suites/noindex.js
