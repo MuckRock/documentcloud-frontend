577f068c6ed82e643cf5b4bba4a9c89d6449bae1
Chris Amico <eyeseast@gmail.com>
2023-12-15 10:14:56 -0500

    Basic action pane stories. Some won't work without state management.

---
+++ src/pages/viewer/pane/stories/SearchPane.stories.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+  import { Story } from "@storybook/addon-svelte-csf";
+  import SearchPane from "../SearchPane.svelte";
+
+  export const meta = {
+    title: "Viewer / Pane / Search Pane",
+    component: SearchPane,
+  };
+</script>
+
+<Story name="default">
+  <SearchPane />
+</Story>
