d1714ef0269cf3bc2a8151805459adf1c0c09f3e
Chris Amico <eyeseast@gmail.com>
2024-01-08 14:45:58 -0500

    really commit

---
+++ src/addons/dispatch/fields/Select.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Field from "./Field.svelte";
+
+  export let title: string;
+  export let name: string;
+  export let defaultValue: string = "";
+  export let value: string = defaultValue || null;
+  export let required: boolean = false;
+  export let description: string = "";
+  export let inline = false;
+  export let choices: string[] = [];
+</script>
+
+<Field {title} {description} {inline} {required}>
+  <select {name} {value}>
+    {#if !required}
+      <option value="">---</option>
+    {/if}
+    {#each choices as choice}
+      <option value={choice}>{choice}</option>
+    {/each}
+  </select>
+</Field>
