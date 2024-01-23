6fab80fdd79bb2f811bbaf7b9257b787ed828741
Allan Lasser <lasser.allan@gmail.com>
2024-01-23 10:03:34 -0500

    Configure Jest to run TS files and tests

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
 /** @type {import('jest').Config} */
 export default {
-  extensionsToTreatAsEsm: [".svelte"],
+  extensionsToTreatAsEsm: [".svelte", ".ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
-  moduleFileExtensions: ["js", "svelte"],
+  moduleFileExtensions: ["js", "ts", "svelte"],
   rootDir: "src",
   setupFiles: ["dotenv/config"],
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.svelte$": ["svelte-jester", { preprocess: true }],
+    "^.+\\.ts$": ["ts-jest", { useESM: true }],
   },
   transformIgnorePatterns: ["/node_modules/(?!svue).+\\.js$"],
   collectCoverageFrom: ["./**/*.{js,svelte}"],
