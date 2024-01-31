0d15989cbbdfb06c6010b4a4f8fe5681d91235fa
Chris Amico <eyeseast@gmail.com>
2024-01-31 13:12:11 -0500

    <Body />

---
+++ src/pages/viewer/stories/Body.stories.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  import { Story } from "@storybook/addon-svelte-csf";
+  import Body from "../Body.svelte";
+  import { Document } from "../../../structure/document.js";
+  import document from "../fixtures/document.json";
+
+  // not clear this does anything useful
+  import { doc } from "@/viewer/document.js";
+  import { viewer } from "../../../viewer/viewer.js";
+
+  export const meta = {
+    title: "Viewer / Body",
+    component: Body,
+    parameters: { layout: "centered" },
+  };
+
+  viewer.doc = new Document(document);
+  doc.viewer = viewer;
+</script>
+
+<Story name="default">
+  <Body />
+</Story>
