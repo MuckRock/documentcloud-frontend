b64c29a689d0f20ac6107563c98ed103c72f17e6
Chris Amico <eyeseast@gmail.com>
2024-02-02 10:48:01 -0500

    Login test

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,13 +1,36 @@
 import { locale, waitLocale } from "svelte-i18n";
 import { browser } from "$app/environment";
+import { BASE_API_URL } from "@/config/config.js";
 import "$lib/i18n/index.js"; // Import to initialize. Important :)
 
 export const trailingSlash = "always";
 
+const endpoint = new URL("users/me/", BASE_API_URL);
+
 /** @type {import('./$types').LayoutLoad} */
-export async function load() {
+export async function load({ fetch }) {
   if (browser) {
     locale.set(window.navigator.language);
   }
   await waitLocale();
+
+  const me = await getMe(fetch);
+
+  return { me };
+}
+
+/**
+ * Get the current logged-in user, or null
+ *
+ * @param {fetch} fetch
+ * @return {*}
+ */
+async function getMe(fetch) {
+  const resp = await fetch(endpoint, { credentials: "include" });
+
+  if (!resp.ok) {
+    return null;
+  }
+
+  return resp.json();
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+  export let data;
+
+  $: me = data.me;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+{#if me}
+  <p>The current logged-in user is {me.name}</p>
+{/if}
