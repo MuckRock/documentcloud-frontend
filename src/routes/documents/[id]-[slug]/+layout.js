ae4743867598b86dcc5ad96dcd53743f840336a6
Chris Amico <eyeseast@gmail.com>
2024-02-08 18:05:05 -0500

    Load notes and sections async

--- src/routes/documents/[id]-[slug]/+layout.js
+++ src/routes/documents/[id]-[slug]/+layout.js
@@ -1,17 +1,13 @@
-import { error, redirect } from "@sveltejs/kit";
-import { BASE_API_URL } from "@/config/config.js";
-import * as documents from "$lib/api/documents.js";
-
-const EXPAND = "user,organization";
-
 /**
- * @type {import('./$types').PageLoad}
- *
- * @export
- *
+ * Load a document for the document viewer.
  * We do this in a layout module because sub-routes can use the same
  * document without loading it again.
  */
+
+import { redirect } from "@sveltejs/kit";
+import * as documents from "$lib/api/documents.js";
+
+/** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
   const document = await documents.get(params.id, fetch);
 
