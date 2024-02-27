6fbe09b2bc825207d72fc159b88f99e7ee12f14b
Allan Lasser <lasser.allan@gmail.com>
2024-02-27 17:46:28 -0500

    Creates MainLayout component

---
+++ src/lib/components/MainLayout.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+  import Logo from "./common/Logo.svelte";
+</script>
+
+<div class="container">
+  <nav class="navigation bar">
+    <header class="header">
+      <a href="/"><Logo /></a>
+    </header>
+    <slot name="navigation" />
+  </nav>
+  <main class="content">
+    <slot name="content" />
+  </main>
+  <nav class="action bar">
+    <header class="header">
+      <p>MuckRock</p>
+    </header>
+    <slot name="action" />
+    <footer>
+      <p>Allan Lasser</p>
+    </footer>
+  </nav>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    gap: 1rem;
+    flex-shrink: 0;
+    background: var(--gray-1, #f5f6f7);
+  }
+  header {
+    display: flex;
+    padding: 1rem;
+    gap: 0.5rem;
+    align-items: flex-start;
+    align-self: stretch;
+    border-bottom: 1px solid var(--gray-2, #d8dee2);
+  }
+  main {
+    display: flex;
+    padding: 4rem 1rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    flex: 1 0 0;
+  }
+  nav {
+    flex: 1 0 0;
+    min-width: 16rem;
+    max-width: 18rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    position: relative;
+  }
+  .navigation {
+    border-right: 1px solid var(--gray-2, #d8dee2);
+  }
+  .action {
+    border-left: 1px solid var(--gray-2, #d8dee2);
+  }
+  .action footer {
+    display: flex;
+    width: 100%;
+    position: absolute;
+    bottom: 0rem;
+    left: 0;
+    right: 0;
+    padding: 1rem;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.875rem;
+    border-top: 1px solid var(--gray-2, #d8dee2);
+    background: var(--gray-1, #f5f6f7);
+    z-index: 1;
+  }
+</style>
---
+++ src/lib/components/stories/MainLayout.stories.svelte
@@ -0,0 +1,30 @@
+<script context="module" lang="ts">
+  import { Story } from "@storybook/addon-svelte-csf";
+  import MainLayout from "../MainLayout.svelte";
+
+  export const meta = {
+    title: "Components / Main Layout",
+    component: MainLayout,
+    parameters: { layout: "fullscreen" },
+  };
+</script>
+
+<Story name="Desktop">
+  <MainLayout>
+    <ul slot="content">
+      <li>Content Item #1</li>
+      <li>Content Item #2</li>
+      <li>Content Item #3</li>
+    </ul>
+    <ul slot="navigation">
+      <li>Nav #1</li>
+      <li>Nav #2</li>
+      <li>Nav #3</li>
+    </ul>
+    <ul slot="action">
+      <li>Action #1</li>
+      <li>Action #2</li>
+      <li>Action #3</li>
+    </ul>
+  </MainLayout>
+</Story>
