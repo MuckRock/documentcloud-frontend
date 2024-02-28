04b3bb05c0c9e24d0e6dc9ae20f47385a9ba7b69
Allan Lasser <lasser.allan@gmail.com>
2024-02-28 14:59:09 -0500

    Create SidebarItem component

---
+++ src/lib/components/SidebarItem.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  export let disabled = false;
+</script>
+
+<div class="container" class:disabled>
+  <slot />
+</div>
+
+<style>
+  .container {
+    display: flex;
+    padding: 0.25rem 0.5rem;
+    align-items: center;
+    gap: 0.5rem;
+    border-radius: 0.25rem;
+
+    color: var(--gray-5, #233944);
+    fill: var(--gray-5, #233944);
+    background: transparent;
+
+    font-size: 1rem;
+    font-weight: var(--font-semibold, 600);
+
+    cursor: pointer;
+  }
+  .container:hover {
+    background: var(--gray-2, #d8dee2);
+  }
+  @media (hover: none) {
+    .container:hover {
+      background: transparent;
+    }
+  }
+  .container.disabled {
+    pointer-events: none;
+    cursor: default;
+    opacity: 0.5;
+  }
+  .container.disabled:hover {
+    background: transparent;
+  }
+</style>
---
+++ src/lib/components/stories/SidebarItem.stories.svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+  import { Story } from "@storybook/addon-svelte-csf";
+  import SidebarItem from "../SidebarItem.svelte";
+  import { Comment16 } from "svelte-octicons";
+
+  export const meta = {
+    title: "Components / Sidebar / Item",
+    component: SidebarItem,
+    parameters: { layout: "centered" },
+  };
+</script>
+
+<Story name="Text Only">
+  <SidebarItem>Example</SidebarItem>
+</Story>
+
+<Story name="Text and Icon">
+  <SidebarItem>
+    <Comment16 /> Add a note…
+  </SidebarItem>
+</Story>
+
+<Story name="Disabled">
+  <SidebarItem disabled>
+    <Comment16 /> Add a note…
+  </SidebarItem>
+</Story>
