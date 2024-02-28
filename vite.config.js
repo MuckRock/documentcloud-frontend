e306a7a8eb2777ba1e4348429f6282a57c25cc19
Chris Amico <eyeseast@gmail.com>
2024-02-28 08:55:04 -0500

    Basic unit test for buttons

--- vite.config.js
+++ vite.config.js
@@ -33,6 +33,7 @@ export default defineConfig({
   },
 
   test: {
+    setupFiles: ["./vitest-setup.js"],
     include: ["src/**/*.{test,spec}.{js,ts}"],
     exclude: [
       ...configDefaults.exclude,
