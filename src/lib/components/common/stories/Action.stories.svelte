3930ed30fdd2e76d424655532197c07978ef2041
Allan Lasser <lasser.allan@gmail.com>
2024-02-28 16:25:49 -0500

    Adds Action component

---
+++ src/lib/components/common/stories/Action.stories.svelte
@@ -0,0 +1,19 @@
+<script context="module" lang="ts">
+  import { Story } from "@storybook/addon-svelte-csf";
+  import { Pencil16 } from "svelte-octicons";
+  import Action from "../Action.svelte";
+
+  export const meta = {
+    title: "Components / Common / Action",
+    component: Action,
+    parameters: { layout: "centered" },
+  };
+</script>
+
+<Story name="Text Only">
+  <Action>Open</Action>
+</Story>
+
+<Story name="Text and Icon">
+  <Action icon={Pencil16}>Edit</Action>
+</Story>
