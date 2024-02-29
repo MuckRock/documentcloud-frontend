8e922adf6d3fe0c9b294e43262ca8c99c9507eb2
Allan Lasser <lasser.allan@gmail.com>
2024-02-29 11:08:02 -0500

    Adds Error common component

---
+++ src/lib/components/common/Error.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { Alert24 } from "svelte-octicons";
+</script>
+
+<div class="container">
+  <Alert24 height={32} width={32} />
+  <slot />
+</div>
+
+<style>
+  .container {
+    display: flex;
+    padding: 1.5rem;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 0.75rem;
+    color: var(--red-dark, #5d275f);
+    fill: var(--red, #e1275f);
+
+    text-align: center;
+    font-size: var(--font-s, 0.75rem);
+  }
+</style>
