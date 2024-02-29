f0a9f8b6eda4fcb32b397afdb72c28eb2780e008
Allan Lasser <lasser.allan@gmail.com>
2024-02-29 11:13:59 -0500

    Update MainLayout in /app

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -10,10 +10,17 @@
     Plug16,
     PlusCircle16,
     Share16,
+    Book16,
   } from "svelte-octicons";
-  import Button from "$lib/components/common/Button.svelte";
   import MainLayout from "$lib/components/MainLayout.svelte";
-  import SidebarItem from "$lib/components/SidebarItem.svelte";
+  import Button from "$lib/components/common/Button.svelte";
+  import SidebarItem from "$lib/components/sidebar/SidebarItem.svelte";
+  import DocumentListItem from "$lib/components/documents/DocumentListItem.svelte";
+  import Flex from "$lib/components/common/Flex.svelte";
+  import SidebarGroup from "$lib/components/sidebar/SidebarGroup.svelte";
+  import Action from "$lib/components/common/Action.svelte";
+  import Pin from "@/common/Pin.svelte";
+  import ResultsList from "@/lib/components/documents/ResultsList.svelte";
 
   export let data;
 
@@ -30,24 +37,65 @@
 
 <MainLayout>
   <svelte:fragment slot="navigation">
-    <SidebarItem><Infinity16 /> {$_("projects.allDocuments")}</SidebarItem>
-    <SidebarItem><Globe16 /> {$_("projects.yourPubDocuments")}</SidebarItem>
-    <SidebarItem><Lock16 /> {$_("projects.yourDocuments")}</SidebarItem>
-    <SidebarItem
-      ><Organization16 />
-      {$_("projects.orgDocuments", {
-        values: { name: "MuckRock" },
-      })}</SidebarItem
-    >
+    <Flex direction="column">
+      <SidebarItem hover
+        ><Infinity16 /> {$_("projects.allDocuments")}</SidebarItem
+      >
+      <SidebarItem hover
+        ><Globe16 /> {$_("projects.yourPubDocuments")}</SidebarItem
+      >
+      <SidebarItem hover><Lock16 /> {$_("projects.yourDocuments")}</SidebarItem>
+      <SidebarItem hover>
+        <Organization16 />
+        {$_("projects.orgDocuments", {
+          values: { name: "MuckRock" },
+        })}
+      </SidebarItem>
+    </Flex>
+    <SidebarGroup>
+      <SidebarItem slot="title"><FileDirectory16 /> Projects</SidebarItem>
+      <Action slot="action" icon={Book16}>Explore</Action>
+      <Flex direction="column" gap={0}>
+        <SidebarItem small href="/project/1">
+          <Pin active /> Oldest Computer
+        </SidebarItem>
+        <SidebarItem small href="/project/2">
+          <Pin active /> FBI Files
+        </SidebarItem>
+        <SidebarItem small href="/project/3">
+          <Pin active /> 1033 Project
+        </SidebarItem>
+      </Flex>
+    </SidebarGroup>
   </svelte:fragment>
   <svelte:fragment slot="content">
-    {searchResults}
+    <ResultsList results={searchResults} />
   </svelte:fragment>
   <svelte:fragment slot="action">
     <Button mode="primary"><PlusCircle16 /> Upload Documents</Button>
-    <SidebarItem disabled><Share16 /> Share…</SidebarItem>
-    <SidebarItem disabled><Pencil16 /> Edit…</SidebarItem>
-    <SidebarItem disabled><FileDirectory16 /> Organize…</SidebarItem>
-    <SidebarItem disabled><Plug16 /> Run…</SidebarItem>
+    <Flex direction="column">
+      <SidebarItem hover disabled><Share16 /> Share…</SidebarItem>
+      <SidebarItem hover disabled><Pencil16 /> Edit…</SidebarItem>
+      <SidebarItem hover disabled><FileDirectory16 /> Organize…</SidebarItem>
+      <SidebarItem hover disabled><Plug16 /> Run…</SidebarItem>
+    </Flex>
+    <SidebarGroup>
+      <SidebarItem slot="title"><Plug16 /> Add-Ons</SidebarItem>
+      <Action slot="action" icon={Book16}>Explore</Action>
+      <Flex direction="column" gap={0}>
+        <SidebarItem small href="/addon/1">
+          <Pin active /> Scraper
+        </SidebarItem>
+        <SidebarItem small href="/addon/2">
+          <Pin active /> Regex Extractor
+        </SidebarItem>
+        <SidebarItem small href="/addon/3">
+          <Pin active /> Tabula Spreadsheet Analysis
+        </SidebarItem>
+        <SidebarItem small href="/addon/4">
+          <Pin active /> GPT 3.5 Analysis
+        </SidebarItem>
+      </Flex>
+    </SidebarGroup>
   </svelte:fragment>
 </MainLayout>
