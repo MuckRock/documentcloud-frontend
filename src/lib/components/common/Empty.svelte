5516918a1d5679ae5fc8dd81d6cfa22966a0bf9f
Allan Lasser <lasser.allan@gmail.com>
2024-02-29 11:07:54 -0500

    Adds Empty common component

---
+++ src/lib/components/common/Empty.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { SvgComponent } from "svelte-octicons";
+
+  export let icon: typeof SvgComponent = null;
+</script>
+
+<div class="container">
+  {#if icon}<svelte:component this={icon} height="32" width="32" />{/if}
+  <slot />
+</div>
+
+<style>
+  .container {
+    display: flex;
+    padding: 1.5rem;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 0.75rem;
+    color: var(--gray-4, #99a8b3);
+    fill: var(--gray-3, #99a8b3);
+
+    text-align: center;
+    font-size: var(--font-s, 0.75rem);
+  }
+</style>
