30649d696a08e49eca3c6bdc57d1955078fea808
Allan Lasser <lasser.allan@gmail.com>
2024-03-28 18:58:07 -0400

    Adds select component

---
+++ src/lib/components/common/InputSelect.svelte
@@ -0,0 +1,74 @@
+<!-- InputSelect.svelte
+  
+  `svelte-select` provides a flexible, customizable basis for the InputSelect component.
+
+  - https://github.com/rob-balfre/svelte-select
+  - https://svelte-select-examples.vercel.app/
+  
+  For data-specific select inputs, like a selection with user avatars,
+  create a custom Select implementation and give it the `select` class.
+-->
+
+<script lang="ts">
+  import Select from "svelte-select";
+  import { ChevronDown16, X16 } from "svelte-octicons";
+
+  export let name: string;
+  export let required = false;
+  export let items: any[];
+  export let value: any = null;
+  export let multiple = false;
+  export let clearable = false;
+  export let placeholder: string = multiple ? "Select multiple" : "Select one";
+</script>
+
+<Select
+  {name}
+  {required}
+  {items}
+  {multiple}
+  {clearable}
+  {placeholder}
+  bind:value
+  showChevron
+  --background="var(--White, #fff)"
+  --border="1px solid var(--gray-3, #99a8b3)"
+  --border-radius="0.5rem"
+  --border-focused="1px solid var(--blue-3, #4294f0)"
+  --item-hover-bg="var(--blue-1, #eef3f9)"
+  --item-is-active-bg="var(--blue-3, #4294f0)"
+  --list-shadow="var(--shadow)"
+  --list-border="1px solid var(--gray-2, #d8dee2)"
+  class="select elevated sourceCodePro gray-4"
+>
+  <ChevronDown16 slot="chevron-icon" />
+  <X16 slot="clear-icon" />
+  <div slot="item" let:item>
+    <p>{item.label}</p>
+  </div>
+</Select>
+
+<style>
+  :global(.select.elevated) {
+    box-shadow: 0px 2px 0px 0px var(--gray-3, #99a8b3);
+  }
+  :global(.select.elevated.focused) {
+    box-shadow: 0px 2px 0px 0px var(--blue-3, #1367d0);
+  }
+  :global(.select .indicators) {
+    fill: var(--gray-5, #233944);
+  }
+  :global(.select.focused .indicators) {
+    fill: var(--blue-5, #053775);
+  }
+  :global(.select input, .select .selected-item) {
+    color: var(--gray-5, #233944);
+    font-family: "Source Sans Pro";
+    font-size: 1rem;
+    font-style: normal;
+    line-height: normal;
+  }
+  :global(.select.focused input, .select.focused .selected-item) {
+    color: var(--blue-5, #053775) !important;
+  }
+</style>
---
+++ src/lib/components/common/stories/InputSelect.stories.svelte
@@ -0,0 +1,35 @@
+<script context="module" lang="ts">
+  import { Template, Story } from "@storybook/addon-svelte-csf";
+  import InputSelect from "../InputSelect.svelte";
+
+  export const meta = {
+    title: "Components / Common / Inputs / Select",
+    component: InputSelect,
+    tags: ["autodocs"],
+  };
+
+  let args = {
+    name: "selectInput",
+    items: [
+      { value: "first", label: "First Choice" },
+      { value: "second", label: "Second Choice" },
+      { value: "third", label: "Third Choice" },
+      { value: "fourth", label: "Fourth Choice" },
+      { value: "fifth", label: "Fifth Choice" },
+      { value: "sixth", label: "Sixth Choice" },
+    ],
+  };
+</script>
+
+<Template let:args>
+  <InputSelect {...args} />
+</Template>
+
+<Story name="Empty" {args} />
+<Story name="Clearable" args={{ ...args, clearable: true }} />
+<Story name="Multiple" args={{ ...args, multiple: true }} />
+<Story
+  name="With Placeholder"
+  args={{ ...args, placeholder: "Go on, make my day…" }}
+/>
+<Story name="With Selection" args={{ ...args, value: "third" }} />
