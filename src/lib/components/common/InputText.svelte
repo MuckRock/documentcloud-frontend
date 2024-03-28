b6b008770c9193a4e5e4b2bc6e09f0a56ca4aefe
Allan Lasser <lasser.allan@gmail.com>
2024-03-28 18:57:59 -0400

    Adds text input component

---
+++ src/lib/components/common/InputText.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let name: string;
+  export let placeholder = "";
+  export let value = "";
+</script>
+
+<input type="text" {name} {placeholder} bind:value />
+
+<style>
+  input {
+    display: flex;
+    padding: 0.375rem 0.75rem;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+
+    border-radius: 0.5rem;
+    border: 1px solid var(--gray-3, hwb(205 60% 30%));
+    background: var(--white, #fff);
+    box-shadow: 0px 2px 0px 0px var(--gray-2, #d8dee2) inset;
+
+    color: var(--gray-5, #233944);
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-family: "Source Sans Pro";
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+  input::placeholder {
+    color: var(--Gray-3, #99a8b3);
+  }
+</style>
---
+++ src/lib/components/common/stories/InputText.stories.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+  import { Template, Story } from "@storybook/addon-svelte-csf";
+  import InputText from "../InputText.svelte";
+
+  export const meta = {
+    title: "Components / Common / Inputs / Text",
+    component: InputText,
+    tags: ["autodocs"],
+    parameters: { layout: "centered" },
+  };
+
+  let args = {
+    name: "textInput",
+  };
+</script>
+
+<Template let:args>
+  <InputText {...args} />
+</Template>
+
+<Story name="Empty" {args} />
+<Story
+  name="With Placeholder"
+  args={{ ...args, placeholder: "Enter some text…" }}
+/>
