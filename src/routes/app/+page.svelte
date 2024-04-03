9eac44c90ef32436e617bfa9570565813a8f44c9
Allan Lasser <lasser.allan@gmail.com>
2024-04-03 19:36:36 -0400

    Moves manager layout out of +layout and into +page

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { _ } from "svelte-i18n";
-  import { Hourglass24 } from "svelte-octicons";
+  import { Hourglass24, PlusCircle16 } from "svelte-octicons";
   import ResultsList, {
     selected,
     total,
@@ -10,6 +10,13 @@
   import PageToolbar from "$lib/components/common/PageToolbar.svelte";
   import Search from "$lib/components/Search.svelte";
   import Empty from "$lib/components/common/Empty.svelte";
+  import Button from "@/lib/components/common/Button.svelte";
+  import MainLayout from "@/lib/components/MainLayout.svelte";
+  import SignedIn from "@/lib/components/common/SignedIn.svelte";
+  import Actions from "./sidebar/Actions.svelte";
+  import AddOns from "./sidebar/AddOns.svelte";
+  import Documents from "./sidebar/Documents.svelte";
+  import Projects from "./sidebar/Projects.svelte";
 
   export let data;
 
@@ -25,44 +32,67 @@
   }
 </script>
 
-<ContentLayout>
-  <PageToolbar slot="header">
-    <Search {query} slot="center" />
-  </PageToolbar>
-  {#await searchResults}
-    <Empty icon={Hourglass24}>Loading…</Empty>
-  {:then results}
-    <ResultsList
-      results={results.results}
-      count={results.count}
-      next={results.next}
-      auto
-    />
-  {/await}
+<MainLayout>
+  <svelte:fragment slot="navigation">
+    <Documents />
+    <SignedIn>
+      <Projects />
+    </SignedIn>
+  </svelte:fragment>
 
-  <PageToolbar slot="footer">
-    <label slot="left" class="select-all">
-      <input
-        type="checkbox"
-        name="select_all"
-        checked={$selected.length > 0 && $selected.length === $visible.size}
-        indeterminate={$selected.length > 0 && $selected.length < $visible.size}
-        on:change={selectAll}
-      />
-      {#if $selected.length > 0}
-        {$selected.length.toLocaleString()} selected
-      {:else}
-        Select all
-      {/if}
-    </label>
+  <svelte:fragment slot="content">
+    <ContentLayout>
+      <PageToolbar slot="header">
+        <Search {query} slot="center" />
+      </PageToolbar>
+      {#await searchResults}
+        <Empty icon={Hourglass24}>Loading…</Empty>
+      {:then results}
+        <ResultsList
+          results={results.results}
+          count={results.count}
+          next={results.next}
+          auto
+        />
+      {/await}
 
-    <svelte:fragment slot="right">
-      {#if $visible && $total}
-        Showing {$visible.size.toLocaleString()} of {$total.toLocaleString()} results
-      {/if}
-    </svelte:fragment>
-  </PageToolbar>
-</ContentLayout>
+      <PageToolbar slot="footer">
+        <label slot="left" class="select-all">
+          <input
+            type="checkbox"
+            name="select_all"
+            checked={$selected.length === $visible.size}
+            indeterminate={$selected.length > 0 &&
+              $selected.length < $visible.size}
+            on:change={selectAll}
+          />
+          {#if $selected.length > 0}
+            {$selected.length.toLocaleString()} selected
+          {:else}
+            Select all
+          {/if}
+        </label>
+
+        <svelte:fragment slot="right">
+          {#if $visible && $total}
+            Showing {$visible.size.toLocaleString()} of {$total.toLocaleString()}
+            results
+          {/if}
+        </svelte:fragment>
+      </PageToolbar>
+    </ContentLayout>
+  </svelte:fragment>
+
+  <svelte:fragment slot="action">
+    <SignedIn>
+      <Button mode="primary" href="/app/upload/"
+        ><PlusCircle16 /> Upload Documents</Button
+      >
+      <Actions />
+      <AddOns pinnedAddOns={data.pinnedAddons} />
+    </SignedIn>
+  </svelte:fragment>
+</MainLayout>
 
 <style>
   label.select-all {
