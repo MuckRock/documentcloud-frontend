854022126eb529207b79c5a9d1065e801af6bb7c
Chris Amico <eyeseast@gmail.com>
2024-04-19 10:29:48 -0400

    barebones text highlights

---
+++ src/lib/components/documents/SearchHighlight.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Highlight } from "$lib/api/types";
+
+  /*
+  {
+  "page_no_1": [
+      ". - Suite 1402 West Tower - Atlanta, GA 30334\nEffective as of\n\n01/31/2016\n\nSherry <em>Boston</em>\nP.O. "
+    ]
+  }
+  */
+  export let highlight: Highlight;
+</script>
+
+{#each Object.entries(highlight) as [page, segments]}
+  <h4>{page}</h4>
+  <blockquote class="highlight">
+    {#each segments as segment}
+      <p class="segment">{@html segment}</p>
+    {/each}
+  </blockquote>
+{/each}
+
+<style>
+  .segment :global(em) {
+    background-color: var(--yellow);
+  }
+</style>
---
+++ src/lib/components/documents/stories/SearchHighlights.stories.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+  import { Story } from "@storybook/addon-svelte-csf";
+  import SearchHighlight from "../SearchHighlight.svelte";
+
+  import highlight from "$lib/api/fixtures/documents/highlight.json";
+
+  export const meta = {
+    title: "Components / Documents / Search Highlight",
+    component: highlight,
+    tags: ["autodocs"],
+    parameters: { layout: "centered" },
+  };
+</script>
+
+<Story name="default">
+  <SearchHighlight {highlight} />
+</Story>
