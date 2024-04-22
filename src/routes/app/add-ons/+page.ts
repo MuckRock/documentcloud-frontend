eea04f3f2cfab016a6a2ce747184295925a9bbf4
Allan Lasser <lasser.allan@gmail.com>
2024-04-22 10:20:00 -0400

    Moves layout logic into page

--- src/routes/app/add-ons/+page.ts
+++ src/routes/app/add-ons/+page.ts
@@ -1,9 +1,14 @@
-import { getAddons } from "@/lib/api/addons.js";
+import { getAddons } from "$lib/api/addons.js";
+import { breadcrumbTrail } from "$lib/utils/navigation";
 
-export async function load({ url, fetch }) {
+export async function load({ url, fetch, parent }) {
+  const breadcrumbs = await breadcrumbTrail(parent, [
+    { href: "/app/add-ons", title: "Add-Ons" },
+  ]);
   const params = Object.fromEntries(url.searchParams.entries());
   const addons = getAddons(params, fetch);
   return {
+    breadcrumbs,
     addons,
   };
 }
