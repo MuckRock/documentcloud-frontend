b331f440cc752fe571cbad24591bcd3ee9f2d7fd
Allan Lasser <lasser.allan@gmail.com>
2024-04-22 17:40:14 -0400

    Update project page

--- src/routes/app/projects/[id]-[slug]/+page.svelte
+++ src/routes/app/projects/[id]-[slug]/+page.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+  import {
+    File24,
+    Hourglass24,
+    Pencil16,
+    People16,
+    Search16,
+    Share16,
+  } from "svelte-octicons";
+
   import MainLayout from "@/lib/components/layouts/MainLayout.svelte";
   import ContentLayout from "@/lib/components/layouts/ContentLayout.svelte";
+  import Divider from "@/lib/components/common/Divider.svelte";
   import Flex from "@/lib/components/common/Flex.svelte";
   import SidebarItem from "@/lib/components/sidebar/SidebarItem.svelte";
-  import { File24, Hourglass24, Pencil16, Search16 } from "svelte-octicons";
-  import { projectSearchUrl } from "@/lib/utils/search";
   import Empty from "@/lib/components/common/Empty.svelte";
-  import DocumentListItem from "@/lib/components/documents/DocumentListItem.svelte";
   import Error from "@/lib/components/common/Error.svelte";
+  import DocumentListItem from "@/lib/components/documents/DocumentListItem.svelte";
+  import { projectSearchUrl } from "@/lib/utils/search";
 
   export let data;
 
@@ -32,7 +41,7 @@
           <DocumentListItem {document} />
         {/if}
       {:else}
-        <Empty icon={File24}>This project is empty</Empty>
+        <Empty icon={File24}>This project has no documents.</Empty>
       {/each}
     {:catch}
       <Error>Error loading project documents</Error>
@@ -40,7 +49,14 @@
   </ContentLayout>
 
   <svelte:fragment slot="action">
-    <SidebarItem href="edit"><Pencil16 /> Edit</SidebarItem>
+    <Flex direction="column">
+      <SidebarItem href="#edit"><Pencil16 /> Edit</SidebarItem>
+      <SidebarItem href="#collaborate"
+        ><People16 /> Manage Collaborators</SidebarItem
+      >
+      <SidebarItem href="#share"><Share16 /> Share & Embed</SidebarItem>
+    </Flex>
+    <Divider />
     <SidebarItem href={projectSearchUrl(project)}>
       <Search16 /> View in Document Search
     </SidebarItem>
