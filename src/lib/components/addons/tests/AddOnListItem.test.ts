90690f21ba5ef50b255d76b8aabdc11b3f562c6c
Allan Lasser <lasser.allan@gmail.com>
2024-04-23 09:52:38 -0400

    Snapshot testing

--- src/lib/components/addons/tests/AddOnListItem.test.ts
+++ src/lib/components/addons/tests/AddOnListItem.test.ts
@@ -6,6 +6,8 @@ import { addon, premiumAddon } from "@/test/fixtures/addons";
 test("AddOnListItem", () => {
   const result = render(AddOnListItem, { addon });
   expect(result.getByRole("heading")).toHaveTextContent(addon.name);
+  expect(result.container).toMatchSnapshot();
   const premium = render(AddOnListItem, { addon: premiumAddon });
   expect(premium.getByRole("status")).toHaveTextContent("Premium");
+  expect(premium.container).toMatchSnapshot();
 });
