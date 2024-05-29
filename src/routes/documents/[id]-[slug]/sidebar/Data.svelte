42490af82fb1da72b8b71c08e39b94c63a06e011
Chris Amico <eyeseast@gmail.com>
2024-05-29 12:32:00 -0400

    Basic tag and KV rendering

--- src/routes/documents/[id]-[slug]/sidebar/Data.svelte
+++ src/routes/documents/[id]-[slug]/sidebar/Data.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import type { Document } from "$lib/api/types";
 
+  import { _ } from "svelte-i18n";
   import { Tag16, Tag24 } from "svelte-octicons";
 
-  import Empty from "@/lib/components/common/Empty.svelte";
-  import KV from "@/lib/components/common/KV.svelte";
-  import SidebarGroup from "@/lib/components/sidebar/SidebarGroup.svelte";
-  import SidebarItem from "@/lib/components/sidebar/SidebarItem.svelte";
+  import Empty from "$lib/components/common/Empty.svelte";
+  import Flex from "$lib/components/common/Flex.svelte";
+  import KV from "$lib/components/common/KV.svelte";
+  import SidebarGroup from "$lib/components/sidebar/SidebarGroup.svelte";
+  import SidebarItem from "$lib/components/sidebar/SidebarItem.svelte";
 
   export let document: Document;
 
@@ -18,13 +20,43 @@
 <SidebarGroup>
   <SidebarItem slot="title">
     <Tag16 />
-    Data & Tags
+    <h3>
+      {$_("sidebar.data.title")}
+    </h3>
   </SidebarItem>
 
   {#if empty}
     <Empty icon={Tag24}>
-      <p>Use tags or key/value data to organize documents</p>
+      <p>{$_("sidebar.data.empty")}</p>
     </Empty>
-    <!-- todo: data and tag components -->
+  {/if}
+
+  {#if tags}
+    <div class="tags">
+      <SidebarItem>
+        <h4>{$_("sidebar.data.tags")}</h4>
+      </SidebarItem>
+
+      <Flex>
+        {#each tags as tag}
+          <KV tag value={tag} />
+        {/each}
+      </Flex>
+    </div>
+  {/if}
+
+  {#if data.length}
+    <div class="data">
+      <SidebarItem>
+        <h4>{$_("sidebar.data.data")}</h4>
+      </SidebarItem>
+      <Flex direction="row">
+        {#each data as [key, values]}
+          {#each values as value}
+            <KV {key} {value} />
+          {/each}
+        {/each}
+      </Flex>
+    </div>
   {/if}
 </SidebarGroup>
