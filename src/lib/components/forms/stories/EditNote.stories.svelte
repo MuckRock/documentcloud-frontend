24f0ae92900b9f8a3b0e813f4b301e0470c4ead2
Chris Amico <eyeseast@gmail.com>
2024-06-28 10:39:18 -0400

    note form

---
+++ src/lib/components/forms/stories/EditNote.stories.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+  import type { Bounds, Document } from "$lib/api/types";
+
+  import { Story } from "@storybook/addon-svelte-csf";
+  import EditNote from "../EditNote.svelte";
+
+  export const meta = {
+    title: "Forms / Edit note",
+    component: EditNote,
+  };
+
+  import doc from "$lib/api/fixtures/documents/document-expanded.json";
+
+  const document = doc as Document;
+  const note = document.notes[0];
+  const coords = [note.x1, note.x2, note.y1, note.y2] as Bounds;
+</script>
+
+<Story name="New note">
+  <EditNote {document} page_number={note.page_number} />
+</Story>
+
+<Story name="Update note">
+  <EditNote {document} {note} {coords} page_number={note.page_number} />
+</Story>
--- src/routes/documents/[id]-[slug]/annotate/+page.svelte
+++ src/routes/documents/[id]-[slug]/annotate/+page.svelte
@@ -7,11 +7,13 @@
   import { setContext } from "svelte";
   import { writable, type Writable } from "svelte/store";
   import { _ } from "svelte-i18n";
+  import { Comment24 } from "svelte-octicons";
 
   import ContentLayout from "$lib/components/layouts/ContentLayout.svelte";
   import Paginator, { currentPage } from "../components/ViewerPaginator.svelte";
   import PageToolbar from "$lib/components/common/PageToolbar.svelte";
   import PDF from "$lib/components/documents/PDF.svelte";
+  import Tip from "$lib/components/common/Tip.svelte";
   import Zoom, { zoom, zoomToScale } from "../components/Zoom.svelte";
 
   import { pageFromHash } from "$lib/api/documents";
@@ -72,6 +74,12 @@
 </svelte:head>
 
 <ContentLayout>
+  <Tip slot="header" --background-color="var(--yellow-bright)">
+    <Comment24 slot="icon" />
+    <h3>{$_("annotate.title")}</h3>
+    <p>{$_("annotate.instructions")}</p>
+  </Tip>
+
   <PDF {document} {asset_url} scale={zoomToScale($zoom)} />
 
   <PageToolbar slot="footer">
