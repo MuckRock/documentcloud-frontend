6360d6eb17be158184df46e6c2cabef690e7ac85
Chris Amico <eyeseast@gmail.com>
2024-07-15 15:37:48 -0400

    breadcrumbs

--- src/lib/components/navigation/tests/Breadcrumbs.test.ts
+++ src/lib/components/navigation/tests/Breadcrumbs.test.ts
@@ -7,15 +7,15 @@ describe("Breadcrumbs", () => {
   it("renders the DocumentCloud logo at the root breadcrumb by default", () => {
     const result = render(Breadcrumbs);
     const links = result.getAllByRole("link");
-    expect(links[0]).toHaveAttribute("href", "/app");
+    expect(links[0]).toHaveAttribute("href", "/");
     expect(links[0].getElementsByTagName("svg")[0]).toEqual(
       render(Logo).container.getElementsByTagName("svg")[0],
     );
   });
   it("renders more breadcrumbs provided in the trail", () => {
     const trail = [
-      { href: "/documents", title: "Documents" },
-      { href: "/documents/[id]", title: "Some Document" },
+      { href: "/documents/", title: "Documents" },
+      { href: "/documents/[id]/", title: "Some Document" },
     ];
     const result = render(Breadcrumbs, { trail });
     const links = result.getAllByRole("link");
