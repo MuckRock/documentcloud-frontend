2c87ebe38df3ac9792e65ff24fd228d7a1f73bde
Allan Lasser <lasser.allan@gmail.com>
2024-07-16 15:33:11 -0400

    Add --font-mono variable

--- src/lib/components/documents/TextPage.svelte
+++ src/lib/components/documents/TextPage.svelte
@@ -25,7 +25,7 @@
     word-break: break-word;
 
     color: var(--gray-5, #233944);
-    font-family: "Source Code Pro", monospace;
+    font-family: var(--font-mono, "Source Code Pro", monospace);
     font-size: calc(var(--font-xs) * var(--zoom, 1));
     line-height: calc(1.25rem * var(--zoom, 1));
     box-shadow: var(--shadow);
