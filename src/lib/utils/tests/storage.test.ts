717be82cd96dbc096fc0def82028fcb742cd86e4
Allan Lasser <lasser.allan@gmail.com>
2024-07-19 18:51:23 -0400

    Adds a storage manager

---
+++ src/lib/utils/tests/storage.test.ts
@@ -0,0 +1,13 @@
+import { test, expect } from 'vitest';
+import { StorageManager } from "../storage";
+
+test("StorageManager", () => {
+  const s = new StorageManager('settings');
+  expect(s.key('property')).toEqual('__documentcloud_settings_property');
+  s.set('property', 12345);
+  expect(s.get('property')).toEqual(12345);
+  expect(s.get('unsetProp', 67890)).toEqual(67890);
+  expect(s.get('unsetProp')).toBeNull();
+  s.remove('property');
+  expect(s.get('property')).toBeNull();
+});
