102a221306d267298d1145801fa8ecedde5cfbaa
Allan Lasser <lasser.allan@gmail.com>
2024-09-13 16:59:22 -0400

    Creates a `PlausibleTracker` component
    Fix attempt #1

---
+++ src/lib/components/common/PlausibleTracker.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Plausible from "plausible-tracker";
+
+  import { getContext, onMount } from "svelte";
+  import type { Writable } from "svelte/store";
+  import { afterNavigate } from "$app/navigation";
+  import { browser } from "$app/environment";
+  import type { User } from "@/lib/api/types";
+  import type { Maybe } from "@/api/types/common";
+
+  let plausible;
+  const user: Writable<Maybe<User>> = getContext("me");
+
+  onMount(() => {
+    if (browser) {
+      plausible = Plausible();
+    }
+  });
+
+  afterNavigate(() => {
+    if (user && browser) {
+      plausible.trackPageview();
+    }
+  });
+</script>
+
+<slot />
