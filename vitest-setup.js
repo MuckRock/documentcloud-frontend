492e038495d49a76d4141ed4b2d407dbfa91ed33
Allan Lasser <lasser.allan@gmail.com>
2024-09-23 15:04:55 -0400

    Polyfill ResizeObserver in vitest

--- vitest-setup.js
+++ vitest-setup.js
@@ -1,3 +1,8 @@
 import "@testing-library/svelte/vitest";
 import "@testing-library/jest-dom/vitest";
 import "./src/lib/i18n/index.js";
+
+import { vi } from "vitest";
+import ResizeObserver from "resize-observer-polyfill";
+
+vi.stubGlobal("ResizeObserver", ResizeObserver);
