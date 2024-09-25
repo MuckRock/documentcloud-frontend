5925425716af6adfc14deca4c31382c66a3d038b
Allan Lasser <lasser.allan@gmail.com>
2024-09-25 08:50:42 -0400

    Additional tests for getApiResponse

--- src/lib/utils/tests/api.test.ts
+++ src/lib/utils/tests/api.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect, vi } from "vitest";
+import { describe, it, test, expect, vi } from "vitest";
 import {
   isErrorCode,
   isRedirectCode,
@@ -31,65 +31,97 @@ describe("response code tests", () => {
   });
 });
 
-describe("api handling", () => {
-  test("getPrivateAsset", async () => {
-    const publicUrl = new URL(
-      "this-is-private.txt",
-      "https://api.www.documentcloud.org",
-    );
+test("getPrivateAsset", async () => {
+  const publicUrl = new URL(
+    "this-is-private.txt",
+    "https://api.www.documentcloud.org",
+  );
 
-    const privateUrl = "https://s3.documentcloud.org/this-is-private.txt";
-    const mockFetch = vi.fn().mockImplementation(async (endpoint, options) => {
-      if (
-        endpoint.toString() === publicUrl.toString() &&
-        options.headers.Accept === "application/json"
-      ) {
-        return {
-          ok: true,
-          status: 200,
-          async json() {
-            return {
-              location: privateUrl,
-            };
-          },
-        };
-      }
-
+  const privateUrl = "https://s3.documentcloud.org/this-is-private.txt";
+  const mockFetch = vi.fn().mockImplementation(async (endpoint, options) => {
+    if (
+      endpoint.toString() === publicUrl.toString() &&
+      options.headers.Accept === "application/json"
+    ) {
       return {
-        status: 302,
-        headers: {
-          Location: privateUrl,
+        ok: true,
+        status: 200,
+        async json() {
+          return {
+            location: privateUrl,
+          };
         },
-        async json() {},
       };
-    });
+    }
 
-    const url = await getPrivateAsset(publicUrl, mockFetch);
-
-    expect(url.href).toEqual(privateUrl);
+    return {
+      status: 302,
+      headers: {
+        Location: privateUrl,
+      },
+      async json() {},
+    };
   });
 
-  test("getApiResponse", async () => {
-    let resp: Response;
+  const url = await getPrivateAsset(publicUrl, mockFetch);
 
-    const body = { test: true };
-    const errors = { error: ["This is an error"] };
+  expect(url.href).toEqual(privateUrl);
+});
+
+describe("getApiResponse", () => {
+  let resp: Response;
+  const body = { test: true };
+  const errors = { error: ["This is an error"] };
+
+  it("returns data for a 200 response", async () => {
     resp = new Response(JSON.stringify(body), { status: 200 });
-
-    let response = await getApiResponse(resp);
-
+    const response = await getApiResponse(resp);
     expect(response.data).toEqual(body);
     expect(response.error).toBeUndefined();
+  });
 
+  it("returns nothing for a 204 response", async () => {
+    resp = new Response(null, { status: 204 });
+    const response = await getApiResponse(resp);
+    expect(response.data).toBeUndefined();
+    expect(response.error).toBeUndefined();
+  });
+
+  it("returns a 500 for a JSON SyntaxError", async () => {
+    resp = new Response("<p>Foo!</p>", { status: 200 });
+    const response = await getApiResponse(resp);
+    expect(response.data).toBeUndefined();
+    expect(response.error).toEqual({
+      status: 500,
+      message: "The API returned invalid JSON",
+    });
+  });
+
+  it("returns a 500 for all other JSON errors", async () => {
+    resp = new Response("{}", { status: 200 });
+    const spy = vi.spyOn(resp, "json").mockImplementation(() => {
+      throw new Error("Unexpected error");
+    });
+    const response = await getApiResponse(resp);
+    expect(response.data).toBeUndefined();
+    expect(response.error).toEqual({
+      status: 500,
+      message: "Error: Unexpected error",
+    });
+    spy.mockRestore();
+  });
+
+  it("returns errors for a 400 response", async () => {
     resp = new Response(JSON.stringify(errors), { status: 400 });
-
-    response = await getApiResponse(resp);
+    const response = await getApiResponse(resp);
     expect(response.data).toBeUndefined();
     expect(response.error.status).toEqual(400);
     expect(response.error.errors).toEqual(errors);
+  });
 
+  it("returns a 500 error for an empty response", async () => {
     // no response is a 500
-    response = await getApiResponse();
+    const response = await getApiResponse();
     expect(response.data).toBeUndefined();
     expect(response.error.status).toEqual(500);
   });
