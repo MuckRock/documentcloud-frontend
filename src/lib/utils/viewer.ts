3afd63746cd9e962a55c75d58cb3412fdecba64f
Allan Lasser <lasser.allan@gmail.com>
2024-09-26 17:25:10 -0400

    Add pageSizes to viewer utils

--- src/lib/utils/viewer.ts
+++ src/lib/utils/viewer.ts
@@ -1,4 +1,5 @@
 import { getContext } from "svelte";
+import type { Writable } from "svelte/store";
 import type { Document, Note, ViewerMode } from "$lib/api/types";
 import { canonicalUrl, pageHashUrl } from "../api/documents";
 import { noteHashUrl } from "../api/notes";
@@ -14,6 +15,10 @@ interface ViewerHrefOptions {
 export function isEmbedded(): boolean {
   // are we embedded?
   return getContext("embed") ?? false;
+}
+
+export function getCurrentPage(): Writable<number> {
+  return getContext("currentPage");
 }
 
 export function getViewerHref(options: ViewerHrefOptions = {}) {
@@ -35,4 +40,36 @@ export function getViewerHref(options: ViewerHrefOptions = {}) {
     if (hash) href += hash;
     return href;
   }
+}
+
+/**
+ * Parse page_spec into width and height of each page
+ *
+ * @param pageSpec A string encoding page dimensions in a compact format
+ * @returns an array of [width, height] tuples
+ */
+export function pageSizes(pageSpec: string): [width: number, height: number][] {
+  // Handle empty page spec
+  if (pageSpec.trim().length == 0) return [];
+
+  const parts = pageSpec.split(";");
+
+  return parts.reduce((sizes, part, i) => {
+    const [size, range] = part.split(":");
+    const [width, height] = size.split("x").map(parseFloat);
+
+    range.split(",").forEach((rangePart) => {
+      if (rangePart.includes("-")) {
+        const [start, end] = rangePart.split("-").map((x) => parseInt(x, 10));
+        for (let page = start; page <= end; page++) {
+          sizes[page] = [width, height];
+        }
+      } else {
+        const page = parseInt(rangePart, 10);
+        sizes[page] = [width, height];
+      }
+    });
+
+    return sizes;
+  }, Array(parts.length));
 }
