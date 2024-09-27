97fa79184353f566c1fb2341c0afc38c19cd788c
Allan Lasser <lasser.allan@gmail.com>
2024-09-27 11:04:40 -0400

    Tests `getViewerHref`

--- src/lib/utils/viewer.ts
+++ src/lib/utils/viewer.ts
@@ -18,21 +18,20 @@ export function isEmbedded(): boolean {
 
 export function getViewerHref(options: ViewerHrefOptions = {}) {
   const { document, page, note, mode = "document", embed = false } = options;
+  let hash = "";
+  if (page) hash = pageHashUrl(page);
+  if (note) hash = noteHashUrl(note);
   if (document) {
     // If we have the document, we can provide an absolute URL
     let url = canonicalUrl(document);
-    if (page) url.hash = pageHashUrl(page);
-    if (note) url.hash = noteHashUrl(note);
     url.searchParams.set("mode", mode);
     if (embed) url.searchParams.set("embed", "1");
+    if (hash) url.hash = hash;
     return url.href;
   } else {
     // If we don't, we can provide a relative URL
     let href = `?mode=${mode}`;
-    let hash: string;
     if (embed) href += "&embed=1";
-    if (page) hash = pageHashUrl(page);
-    if (note) hash = noteHashUrl(note);
     if (hash) href += hash;
     return href;
   }
