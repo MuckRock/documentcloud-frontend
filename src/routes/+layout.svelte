674e3bd240fb2b7e5723766c6747e9b47eb4120b
Allan Lasser <lasser.allan@gmail.com>
2024-10-01 16:34:06 -0400

    Loads fonts based on locale

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,18 @@
   import "@/style/variables.css";
   import "@/style/global.css";
   import "@/style/kit.css";
+
+  import { locale } from "svelte-i18n";
+
+  $: useCyrillicCharset = ["uk", "ru"].includes($locale);
 </script>
+
+<svelte:head>
+  {#if useCyrillicCharset}
+    <link rel="stylesheet" href="/fonts-cyrillic.css" />
+  {:else}
+    <link rel="stylesheet" href="/fonts-latin.css" />
+  {/if}
+</svelte:head>
 
 <slot />
