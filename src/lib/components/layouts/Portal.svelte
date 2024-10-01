ac8cc1e57e6faa9dafe267601b79ed5d22cf4c78
Allan Lasser <lasser.allan@gmail.com>
2024-10-01 12:14:34 -0400

    Remove Portal from DOM before a navigation change Fixes #742

--- src/lib/components/layouts/Portal.svelte
+++ src/lib/components/layouts/Portal.svelte
@@ -2,7 +2,8 @@
 <!-- Source: https://github.com/sveltejs/svelte/issues/3088#issuecomment-1065827485 -->
 
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
+  import { onMount, onDestroy } from "svelte";
+  import { beforeNavigate } from "$app/navigation";
 
   export let target: HTMLElement | null | undefined = globalThis.document?.body;
 
@@ -12,7 +13,13 @@
     if (target) {
       target.appendChild(ref);
     }
-  })
+  });
+
+  beforeNavigate(() => {
+    if (ref?.parentNode) {
+      ref.parentNode?.removeChild(ref);
+    }
+  });
 
   // this block is almost needless/useless (if not totally) as, on destroy, the ref will no longer exist/be in the DOM anyways
   onDestroy(() => {
@@ -20,8 +27,8 @@
       if (ref?.parentNode) {
         ref.parentNode?.removeChild(ref);
       }
-    })
-  })
+    });
+  });
 </script>
 
 <div bind:this={ref} style="display:contents;">
