d84895a89601006a6a69336f584c99a49246aee9
Allan Lasser <lasser.allan@gmail.com>
2024-10-09 10:03:46 -0400

    Adds fns to sort users and get sorted org users

--- src/lib/api/tests/accounts.test.ts
+++ src/lib/api/tests/accounts.test.ts
@@ -19,6 +19,8 @@ import {
   getCreditBalance,
   userOrgs,
   orgUsers,
+  alphabetizeUsers,
+  inMyOrg,
 } from "../accounts";
 
 describe("getMe", async () => {
@@ -218,4 +220,87 @@ test("getCreditBalance", () => {
   expect(getCreditBalance(null)).toBeNull();
   expect(getCreditBalance(fixtures.freeOrg)).toBe(0);
   expect(getCreditBalance(fixtures.proOrg)).toBe(5500);
+});
+
+test("alphabetizeUsers", () => {
+  const unorderedList = [
+    { name: "Tony Danza", username: "td" },
+    { name: "Henry Winkler", username: "hw" },
+    { username: "tk" },
+  ];
+  expect(unorderedList.sort(alphabetizeUsers)).toEqual([
+    { name: "Henry Winkler", username: "hw" },
+    { username: "tk" },
+    { name: "Tony Danza", username: "td" },
+  ]);
+});
+
+test("inMyOrg", () => {
+  const org = fixtures.organization;
+  const users = fixtures.usersList.results.filter((user) =>
+    user.organizations.includes(org.id),
+  );
+  const me = fixtures.me;
+  expect(inMyOrg(org.id, me.id, users)).toEqual([
+    {
+      id: 3,
+      avatar_url:
+        "https://squarelet-staging.s3.amazonaws.com/media/account_images/Screenshot_2016-11-16_15.43.27.png",
+      name: "Michael Morisy",
+      organization: 1,
+      organizations: [7, 9, 1, 6, 4, 8, 5],
+      admin_organizations: [7, 9, 1, 6, 4, 8, 5],
+      username: "morisy",
+      uuid: "a7579bae-6e11-4ef0-8d65-e60288fba420",
+      verified_journalist: true,
+    },
+    {
+      id: 1,
+      avatar_url:
+        "https://squarelet-staging.s3.amazonaws.com/media/account_images/22_MuckRock_2018_PRELIMANRY_EDITS_DSC_5387_2018_Derek_Kouyoumjian_preview_SfuGGnJ.jpg",
+      name: "Mitchell J Kotler",
+      organization: 1,
+      organizations: [10028, 10027, 2, 1],
+      admin_organizations: [10028, 10027, 2, 1],
+      username: "mitch",
+      uuid: "76742194-b998-4ca5-a7af-fd054de89e88",
+      verified_journalist: true,
+    },
+    {
+      id: 2,
+      avatar_url:
+        "https://squarelet-staging.s3.amazonaws.com/static/images/avatars/profile.png",
+      name: "Dylan Freedman",
+      organization: 3,
+      organizations: [3, 1],
+      admin_organizations: [3],
+      username: "freedmand",
+      uuid: "0aa27e3b-c5d8-4a9d-a278-753a770f86d7",
+      verified_journalist: true,
+    },
+    {
+      id: 100010,
+      avatar_url:
+        "https://squarelet-staging.s3.amazonaws.com/media/avatars/ABS.jpg",
+      name: "ja-test",
+      organization: 10021,
+      organizations: [10021, 1],
+      admin_organizations: [10021],
+      username: "ja22",
+      uuid: "78756937-b6d9-40ee-b1ac-03c134416de6",
+      verified_journalist: true,
+    },
+    {
+      id: 100001,
+      avatar_url:
+        "https://squarelet-staging.s3.amazonaws.com/static/images/avatars/profile.png",
+      name: "Muckrock Staff",
+      organization: 1,
+      organizations: [1, 10002],
+      admin_organizations: [10002],
+      username: "MuckrockStaff",
+      uuid: "0cbb557a-344e-4da7-9d4c-b9bf8e0a899a",
+      verified_journalist: true,
+    },
+  ]);
 });
