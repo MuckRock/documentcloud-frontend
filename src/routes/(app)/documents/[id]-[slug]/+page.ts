2daccf0f10376347de8a8e6f86bb953c55d624b6
Allan Lasser <lasser.allan@gmail.com>
2024-10-10 14:26:27 -0400

    Abstract search query loading

--- src/routes/(app)/documents/[id]-[slug]/+page.ts
+++ src/routes/(app)/documents/[id]-[slug]/+page.ts
@@ -1,25 +1,10 @@
-import type { DocumentText } from "$lib/api/types.js";
+import { getQuery } from "@/lib/utils/search.js";
 
-import * as documents from "$lib/api/documents";
-
-export async function load({ fetch, parent, url, data, depends }) {
-  const query = url.searchParams.get("q") ?? "";
-
-  const { document, mode } = await parent();
-
-  // load text
-  let text = await documents.text(document, fetch);
-
-  const asset_url = await documents.assetUrl(document, fetch);
-
-  // so we can reload when we reprocess
-  depends(`document:${document.id}`);
+export async function load({ url, data }) {
+  const query = getQuery(url);
 
   return {
     ...(data ?? {}), // include csrf_token
-    asset_url,
     query,
-    text,
-    mode,
   };
 }
--- src/routes/embed/documents/[id]-[slug]/+page.ts
+++ src/routes/embed/documents/[id]-[slug]/+page.ts
@@ -1,21 +1,10 @@
-import type { DocumentText, ViewerMode } from "@/lib/api/types";
-// load data for a single page embed
-import * as documents from "@/lib/api/documents";
-import * as notesApi from "$lib/api/notes";
+import { getQuery } from "@/lib/utils/search.js";
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ parent, fetch, url }) {
-  const { document } = await parent();
-
-  const query = url.searchParams.get("q") ?? "";
-
-  // load text
-  const text = await documents.text(document, fetch);
-  const asset_url = await documents.assetUrl(document, fetch);
+export async function load({ url }) {
+  const query = getQuery(url);
 
   return {
-    asset_url,
     query,
-    text,
   };
 }
