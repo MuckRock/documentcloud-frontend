57f055c9401daae6ef2acde95ce1dc655a215508
Chris Amico <eyeseast@gmail.com>
2024-10-16 12:23:54 -0400

    move to top

--- src/lib/components/projects/Collaborators.svelte
+++ src/lib/components/projects/Collaborators.svelte
@@ -10,6 +10,8 @@
   import { Pencil16, People16, PlusCircle16, XCircle16 } from "svelte-octicons";
 
   import Action from "../common/Action.svelte";
+  import Flex from "../common/Flex.svelte";
+  import Button from "../common/Button.svelte";
   import Empty from "../common/Empty.svelte";
   import SidebarGroup from "../sidebar/SidebarGroup.svelte";
   import SidebarItem from "../sidebar/SidebarItem.svelte";
@@ -74,42 +76,49 @@
     {$_("projects.collaborators.title")}
   </SidebarItem>
 
+  {#if users.length > 0 && project.add_remove_access}
+    <SidebarItem hover small on:click={() => (show = "invite")}>
+      <PlusCircle16 slot="start" />
+      {$_("projects.collaborators.add")}
+    </SidebarItem>
+  {/if}
+
   {#each sort(users) as user}
     {#if isProjectUser || project.edit_access}
       <SidebarItem small>
         <Avatar user={user.user} slot="start" />
         {getUserName(user.user)}
-      <Flex slot="end">
-        <span class="badge">{$_(accessLabels[user.access])}</span>
-        {#if project.add_remove_access && !isMe(user, $me)}
-          <Button
-            ghost
-            mode="primary"
-            minW={false}
-            size="small"
-            title={actions.update}
-            on:click={() => {
-              user_to_update = user;
-              show = "update";
-            }}
-          >
-            <Pencil16 />
-          </Button>
-          <Button
-            ghost
-            mode="danger"
-            minW={false}
-            size="small"
-            title={actions.remove}
-            on:click={() => {
-              user_to_update = user;
-              show = "remove";
-            }}
-          >
-            <XCircle16 />
-          </Button>
-        {/if}
-      </Flex>
+        <Flex slot="end">
+          <span class="badge">{$_(accessLabels[user.access])}</span>
+          {#if project.add_remove_access && !isMe(user, $me)}
+            <Button
+              ghost
+              mode="primary"
+              minW={false}
+              size="small"
+              title={actions.update}
+              on:click={() => {
+                user_to_update = user;
+                show = "update";
+              }}
+            >
+              <Pencil16 />
+            </Button>
+            <Button
+              ghost
+              mode="danger"
+              minW={false}
+              size="small"
+              title={actions.remove}
+              on:click={() => {
+                user_to_update = user;
+                show = "remove";
+              }}
+            >
+              <XCircle16 />
+            </Button>
+          {/if}
+        </Flex>
       </SidebarItem>
     {:else}
       <SidebarItem small>
@@ -127,13 +136,6 @@
       {/if}
     </Empty>
   {/each}
-
-  {#if users.length > 0 && project.add_remove_access}
-    <SidebarItem hover small on:click={() => (show = "invite")}>
-      <PlusCircle16 slot="start" />
-      {$_("projects.collaborators.add")}
-    </SidebarItem>
-  {/if}
 </SidebarGroup>
 
 {#if show}
