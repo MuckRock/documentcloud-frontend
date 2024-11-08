ff6c296b587b113f7aacfa3f3167b9bdcd4b3ba4
Chris Amico <eyeseast@gmail.com>
2024-11-08 11:47:24 -0500

    fix a test

--- src/lib/components/addons/tests/AddOnListItem.test.ts
+++ src/lib/components/addons/tests/AddOnListItem.test.ts
@@ -6,9 +6,9 @@ import { addon, premiumAddon } from "@/test/fixtures/addons";
 
 test("AddOnListItem", () => {
   const result = render(AddOnListItem, { addon });
-  expect(result.getByRole("heading")).toHaveTextContent(addon.name);
+  expect(result.getByRole("heading").textContent).toEqual(addon.name);
   expect(result.container).toMatchSnapshot();
   const premium = render(AddOnListItem, { addon: premiumAddon });
-  expect(premium.getByRole("status")).toHaveTextContent("Premium");
+  expect(premium.getByRole("status").textContent).toContain("Premium");
   expect(premium.container).toMatchSnapshot();
 });
