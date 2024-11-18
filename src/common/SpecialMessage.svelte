93de140d40baa51e64c3b515e76688da8ebd661a
Chris Amico <eyeseast@gmail.com>
2024-11-18 18:19:15 -0500

    a very special message

--- src/common/SpecialMessage.svelte
+++ src/common/SpecialMessage.svelte
@@ -1,49 +1,19 @@
 <script>
   import { _ } from "svelte-i18n";
-  import { onMount } from "svelte";
-  import { baseApiUrl } from "../api/base.js";
-  import { SPECIAL_VERSION, SPECIAL_CONTACT } from "../config/config.js";
-
-  const endpoint = new URL("/api/flatpages/tipofday/", baseApiUrl);
-
-  let loading;
-
-  async function load() {
-    const resp = await fetch(endpoint);
-
-    if (!resp.ok) {
-      return "";
-    }
-
-    const { content } = await resp.json();
-
-    return content;
-  }
-
-  onMount(() => {
-    loading = load();
-  });
-
-  $: show = SPECIAL_VERSION != null && SPECIAL_VERSION.trim().length > 0;
-  $: showContact = SPECIAL_CONTACT != null && SPECIAL_CONTACT.trim().length > 0;
+  import { SPECIAL_CONTACT } from "../config/config.js";
 </script>
 
-{#if show}
-  <div class="container">
-    <div class="special">
-      {#await loading then content}
-        {@html content}
-      {:catch}
-        {SPECIAL_VERSION}
-        {#if showContact}
-          {@html $_("specialMessage.constactUs", {
-            values: { contact: SPECIAL_CONTACT },
-          })}
-        {/if}
-      {/await}
-    </div>
+<div class="container">
+  <div class="special">
+    <p>
+      This is the legacy version of DocumentCloud. Check out the new and
+      improved version at <a href="https://www.documentcloud.org">
+        https://www.documentcloud.org
+      </a>
+      or <a href={SPECIAL_CONTACT}>send us feedback</a>.
+    </p>
   </div>
-{/if}
+</div>
 
 <style>
   .container {
