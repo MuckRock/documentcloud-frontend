266f7fd90e678b603ff0d231a533289f36a52a80
Chris Amico <eyeseast@gmail.com>
2024-11-26 20:46:52 -0500

    documents

--- src/routes/(app)/documents/+layout.ts
+++ src/routes/(app)/documents/+layout.ts
@@ -1,9 +1,18 @@
 import { breadcrumbTrail } from "$lib/utils/navigation";
 
-export async function load({ parent }) {
+import { VIEWER_MAX_AGE } from "@/config/config.js";
+
+export async function load({ parent, setHeaders }) {
+  const { me } = await parent();
   const breadcrumbs = await breadcrumbTrail(parent, [
     { href: "/documents/", title: "Documents" },
   ]);
+
+  if (!me) {
+    setHeaders({
+      "max-age": `public, max-age=${VIEWER_MAX_AGE}`,
+    });
+  }
 
   return {
     breadcrumbs,
