a650af3ff6f42804c130b9141dd178125a3cc4ce
Chris Amico <eyeseast@gmail.com>
2024-11-26 21:28:00 -0500

    Production URLs

--- src/config/production.js
+++ src/config/production.js
@@ -1,6 +1,6 @@
 export const DC_BASE = "https://api.www.documentcloud.org";
-export const APP_URL = "https://next.www.documentcloud.org/";
-export const EMBED_URL = "https://next.www.documentcloud.org/";
+export const APP_URL = "https://www.documentcloud.org/";
+export const EMBED_URL = "https://embed.documentcloud.org/";
 export const SQUARELET_BASE = "https://accounts.muckrock.com";
 export const STAFF_ONLY_S3_URL =
   "https://s3.console.aws.amazon.com/s3/buckets/s3.documentcloud.org?region=us-east-1&prefix=documents/$$ID$$/&showversions=false";
