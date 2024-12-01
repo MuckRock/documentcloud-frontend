ff19e26bd69dc40e73701acd1acdbd861e5dce20
Chris Amico <eyeseast@gmail.com>
2024-12-01 10:35:59 -0500

    Header is cache-control, not max-age

--- src/routes/(app)/documents/+layout.ts
+++ src/routes/(app)/documents/+layout.ts
@@ -1,18 +1,9 @@
 import { breadcrumbTrail } from "$lib/utils/navigation";
 
-import { VIEWER_MAX_AGE } from "@/config/config.js";
-
-export async function load({ parent, setHeaders }) {
-  const { me } = await parent();
+export async function load({ parent }) {
   const breadcrumbs = await breadcrumbTrail(parent, [
     { href: "/documents/", title: "Documents" },
   ]);
-
-  if (!me) {
-    setHeaders({
-      "max-age": `public, max-age=${VIEWER_MAX_AGE}`,
-    });
-  }
 
   return {
     breadcrumbs,
