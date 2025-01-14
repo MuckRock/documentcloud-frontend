6a83857163da964f8342165fbf883eb915e7b754
Chris Amico <eyeseast@gmail.com>
2025-01-14 12:33:00 -0500

    Bust cache and fix canonical link

--- src/routes/(app)/projects/[id]/+page.ts
+++ src/routes/(app)/projects/[id]/+page.ts
@@ -6,12 +6,12 @@ import * as projects from "$lib/api/projects";
 export async function load({ params, fetch }) {
   const { data: project, error: err } = await projects.get(+params.id, fetch);
 
-  if (!project) {
-    return error(404, "Project not found");
-  }
-
   if (err) {
     return error(err.status, err.message);
+  }
+
+  if (!project) {
+    return error(404, "Project not found");
   }
 
   const url = projects.canonicalUrl(project);
