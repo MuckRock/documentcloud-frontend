f9f05814245c26a2240c6c989042b1a6f4f7ce38
Chris Amico <eyeseast@gmail.com>
2025-02-06 10:21:45 -0500

    me is a store

--- src/lib/components/layouts/Error.svelte
+++ src/lib/components/layouts/Error.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import type { Maybe } from "$lib/api/types";
+
+  import { page } from "$app/stores";
+
   import { _ } from "svelte-i18n";
 
   import Button from "../common/Button.svelte";
@@ -7,10 +11,8 @@
   import Portal from "./Portal.svelte";
   import UserFeedback from "../forms/UserFeedback.svelte";
 
-  import { page } from "$app/stores";
-  import type { Maybe } from "$lib/api/types";
   import { getCurrentUser } from "$lib/utils/permissions";
-  import { SIGN_IN_URL } from "@/config/config";
+  import { SIGN_IN_URL } from "@/config/config.js";
 
   const me = getCurrentUser();
 
@@ -32,7 +34,7 @@
     <slot name="message" />
   </Error>
   <slot />
-  {#if status === 404 && !me}
+  {#if status === 404 && !$me}
     <p class="signInMessage">
       {@html $_("error.signIn", { values: { href: sign_in_url.href } })}
     </p>
