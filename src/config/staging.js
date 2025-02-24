bcb0793e26f700e51cc85c78ffff7f1ef2225175
Chris Amico <eyeseast@gmail.com>
2025-02-24 12:48:39 -0500

    Trying to catch errant process

--- src/config/staging.js
+++ src/config/staging.js
@@ -1,5 +1,7 @@
 export const DC_BASE = "https://api.muckcloud.com";
-export const APP_URL = process.env.DEPLOY_PRIME_URL || "https://muckcloud.com/";
+export const APP_URL = globalThis.process
+  ? (process?.env?.DEPLOY_PRIME_URL ?? "https://muckcloud.com/")
+  : "https://muckcloud.com";
 export const EMBED_URL = APP_URL;
 export const SQUARELET_BASE = "https://squarelet-staging.herokuapp.com";
 export const STAFF_ONLY_S3_URL =
