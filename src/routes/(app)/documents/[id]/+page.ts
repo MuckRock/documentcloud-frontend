6526ce744e680138e76d29dcf17e60a675e11eb8
Chris Amico <eyeseast@gmail.com>
2025-05-05 11:23:14 -0400

    Add redirects for bare ID routes

---
+++ src/routes/(app)/documents/[id]/+page.ts
@@ -0,0 +1,22 @@
+// redirect /documents/id/ to /documents/id-slug/
+import { error, redirect } from "@sveltejs/kit";
+import * as documents from "$lib/api/documents";
+
+// save a redirect
+export const trailingSlash = "ignore";
+
+/** @type {import('./$types').PageLoad} */
+export async function load({ params, fetch, url }) {
+  const { data: document, error: err } = await documents.get(+params.id, fetch);
+  if (err) {
+    console.warn(err.status, url.href);
+    return error(err.status, err.message);
+  }
+
+  if (!document) {
+    return error(404, "Document not found");
+  }
+  const canonical = documents.canonicalUrl(document);
+
+  return redirect(308, canonical);
+}
