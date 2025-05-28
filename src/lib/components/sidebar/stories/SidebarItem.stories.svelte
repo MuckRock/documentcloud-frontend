ee0611117bc186e3de391dc59b4e9b0f4460ffe5
Chris Amico <eyeseast@gmail.com>
2025-05-28 11:47:07 -0400

    Set a big (and configurable) max width in SidebarItem

--- src/lib/components/sidebar/stories/SidebarItem.stories.svelte
+++ src/lib/components/sidebar/stories/SidebarItem.stories.svelte
@@ -81,6 +81,17 @@
   </div>
 </Story>
 
+<Story name="Absurdly long name">
+  <SidebarItem>
+    The International Consortium for the Real-Time Dissemination of Verified,
+    Unbiased, Cross-Platform, Community-Sourced, Multilingual, Deep-Dive
+    Investigative Journalism and Global News Analysis Network for Socioeconomic,
+    Environmental, Political, Technological, and Humanitarian Affairs Reporting
+    and Strategic Public Information Engagement Across All Media Channels
+    Initiative
+  </SidebarItem>
+</Story>
+
 <style>
   .maxW-16 {
     max-width: 16rem;
