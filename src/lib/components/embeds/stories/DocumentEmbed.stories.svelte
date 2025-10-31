b9bf29f20ab2f31e82c6493efda64297e7350992
Chris Amico <eyeseast@gmail.com>
2025-10-31 08:52:29 -0400

    Switch documents

--- src/lib/components/embeds/stories/DocumentEmbed.stories.svelte
+++ src/lib/components/embeds/stories/DocumentEmbed.stories.svelte
@@ -2,12 +2,10 @@
   import { Template, Story } from "@storybook/addon-svelte-csf";
   import DocumentEmbed from "../DocumentEmbed.svelte";
 
-  import doc from "@/test/fixtures/documents/document-expanded.json";
   import EmbedLayout from "../../layouts/EmbedLayout.svelte";
+  import ViewerContext from "../../viewer/ViewerContext.svelte";
   import { canonicalUrl } from "$lib/api/documents";
-  import type { Document } from "$lib/api/types";
-
-  const document = doc as Document;
+  import { documentExpanded as document } from "@/test/fixtures/documents";
 
   let args = {
     document,
@@ -23,13 +21,16 @@
 
 <Template let:args>
   <div class="vh-100">
-    <EmbedLayout canonicalUrl={canonicalUrl(document).href}>
-      <DocumentEmbed {...args} />
+    <EmbedLayout canonicalUrl={canonicalUrl(args.document).href}>
+      <ViewerContext document={args.document}>
+        <DocumentEmbed />
+      </ViewerContext>
     </EmbedLayout>
   </div>
 </Template>
 
 <Story name="Public" {args} />
+
 <Story
   name="Private"
   args={{ ...args, document: { ...document, access: "private" } }}
