e7bdf6c8ce43ca89e451005aed0962a2c4c001fb
Chris Amico <eyeseast@gmail.com>
2025-12-09 16:18:06 -0500

    home

--- src/routes/(pages)/home/+page.ts
+++ src/routes/(pages)/home/+page.ts
@@ -5,6 +5,7 @@ import { PAGE_MAX_AGE } from "@/config/config.js";
 import * as flatpages from "$lib/api/flatpages";
 import { getMe } from "$lib/api/accounts";
 import { renderMarkdown } from "$lib/utils/markup";
+import { ALLOWED_TAGS } from "@/config/config.js";
 
 export const trailingSlash = "ignore";
 
@@ -31,7 +32,9 @@ export async function load({ fetch, setHeaders }) {
   return {
     title: page.title,
     url: page.url,
-    content: renderMarkdown(page.content),
+    content: renderMarkdown(page.content, {
+      allowedTags: ALLOWED_TAGS.concat("h1", "h2", "h3", "h4", "h5", "h6"),
+    }),
     me,
   };
 }
