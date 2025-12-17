70e91f5538ee724bbf8d170b625a95cbecbada69
Chris Amico <eyeseast@gmail.com>
2025-12-17 09:10:01 -0500

    Upload doesn't overwrite csrf token now. Tests pass.

--- src/lib/components/forms/tests/Upload.test.ts
+++ src/lib/components/forms/tests/Upload.test.ts
@@ -1,7 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { writable } from "svelte/store";
 import {
-  act,
   render,
   screen,
   fireEvent,
@@ -16,36 +15,40 @@ import { me } from "@/test/fixtures/accounts";
 describe("DocumentUpload form", () => {
   it("lists files selected for upload", async () => {
     render(DocumentUploadForm, { user: writable(me), csrf_token: "token" });
-    const dropElement = screen.getByText(
-      "Select, paste or drag-and-drop files to upload",
-    );
-    const dropEvent = createEvent.drop(dropElement);
-    const fileList = [new File([new ArrayBuffer(128000)], "file.pdf")];
+    const dropzone = document.querySelector(
+      '[aria-dropeffect="execute"]',
+    ) as HTMLElement;
+    const dropEvent = createEvent.drop(dropzone);
     Object.defineProperty(dropEvent, "dataTransfer", {
       value: {
-        files: fileList,
+        files: [
+          new File([new ArrayBuffer(128000)], "file.pdf", {
+            type: "application/pdf",
+          }),
+        ],
       },
     });
-    await act(() => fireEvent(dropElement, dropEvent));
+    fireEvent(dropzone, dropEvent);
     const fileListItem = await waitFor(() => screen.getByRole("listitem"));
     expect(fileListItem.textContent).toContain("128 kB");
   });
 
   it("provides feedback when a file is too large", async () => {
     render(DocumentUploadForm, { user: writable(me), csrf_token: "token" });
-    const dropElement = screen.getByText(
-      "Select, paste or drag-and-drop files to upload",
-    );
-    const dropEvent = createEvent.drop(dropElement);
-    const fileList = [
-      new File([new ArrayBuffer(PDF_SIZE_LIMIT + 1)], "file.pdf"),
-    ];
+    const dropzone = document.querySelector(
+      '[aria-dropeffect="execute"]',
+    ) as HTMLElement;
+    const dropEvent = createEvent.drop(dropzone);
     Object.defineProperty(dropEvent, "dataTransfer", {
       value: {
-        files: fileList,
+        files: [
+          new File([new ArrayBuffer(PDF_SIZE_LIMIT + 1)], "file.pdf", {
+            type: "application/pdf",
+          }),
+        ],
       },
     });
-    await act(() => fireEvent(dropElement, dropEvent));
+    fireEvent(dropzone, dropEvent);
     const fileListItem = await waitFor(() => screen.getByRole("listitem"));
     expect(fileListItem.textContent).toContain(
       "The maximum size for a PDF is 500MB",
