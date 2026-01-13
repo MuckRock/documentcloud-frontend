3f6e8f228aaa8f5ecd46ddf36b9ba596150cca95
Chris Amico <eyeseast@gmail.com>
2026-01-13 15:29:31 -0500

    Update add-on routes to Svelte 5

--- src/routes/(app)/add-ons/[owner]/[repo]/[event]/+page.svelte
+++ src/routes/(app)/add-ons/[owner]/[repo]/[event]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { afterNavigate } from "$app/navigation";
   import { _ } from "svelte-i18n";
 
   import AddOnLayout from "$lib/components/layouts/AddOnLayout.svelte";
@@ -6,29 +7,35 @@
   import { values } from "$lib/components/forms/AddOnDispatch.svelte";
   import { schedules } from "$lib/components/addons/ScheduledEvent.svelte";
   import { isPremiumOrg, getCreditBalance } from "$lib/api/accounts";
-  import { getCurrentUser } from "$lib/utils/permissions.js";
+  import { getCurrentUser } from "$lib/utils/permissions";
 
-  export let data;
+  let { data } = $props();
 
   const me = getCurrentUser();
 
-  $: event = data.event;
-  $: addon = data.event.addon;
-  $: query = data.query;
-  $: search = data.searchResults;
-  $: scheduled = data.scheduled;
-  $: organization =
-    typeof $me?.organization === "object" ? $me.organization : null;
-  $: isPremiumUser = isPremiumOrg(organization);
-  $: creditBalance = getCreditBalance(organization) ?? 0;
-  $: isPremiumAddon =
-    addon?.parameters?.categories?.includes("premium") ?? false;
-  $: disablePremium = isPremiumAddon && (!isPremiumUser || creditBalance === 0);
-  $: history = data.history;
+  let event = $derived(data.event);
+  let addon = $derived(data.event.addon);
+  let query = $derived(data.query);
+  let search = $derived(data.searchResults);
+  let scheduled = $derived(data.scheduled);
+  let organization = $derived(
+    typeof $me?.organization === "object" ? $me.organization : null,
+  );
+  let isPremiumUser = $derived(isPremiumOrg(organization));
+  let creditBalance = $derived(getCreditBalance(organization) ?? 0);
+  let isPremiumAddon = $derived(
+    addon?.parameters?.categories?.includes("premium") ?? false,
+  );
+  let disablePremium = $derived(
+    isPremiumAddon && (!isPremiumUser || creditBalance === 0),
+  );
+  let history = $derived(data.history);
 
   // set initial form values when route changes
-  $: values.update((v) => {
-    return { ...v, ...event.parameters, event: schedules[event.event] };
+  afterNavigate(() => {
+    values.update((v) => {
+      return { ...v, ...event.parameters, event: schedules[event.event] };
+    });
   });
 </script>
 
