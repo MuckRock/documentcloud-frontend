a5dd54fe1a70198abfd062e2c19f1a7853f41762
Chris Amico <eyeseast@gmail.com>
2026-01-22 22:32:01 -0500

    very long

--- src/lib/components/processing/stories/Documents.stories.svelte
+++ src/lib/components/processing/stories/Documents.stories.svelte
@@ -10,7 +10,7 @@
     component: Documents,
     parameters: {
       layout: "centered",
-      delay: 15000, // give the mock time to load
+      delay: 30_000, // give the mock time to load
     },
   };
 </script>
