702247982d87ce5c2cf3416ae8249d6b98ba431d
Daniel Nass <dnlnss@gmail.com>
2026-06-16 12:26:55 -0400

    Add donation form component

---
+++ src/lib/components/home/DonationForm.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let { id }: { id: string } = $props();
+</script>
+
+<svelte:head>
+  <script
+    async
+    src="https://widgets.givebutter.com/latest.umd.cjs?acct=bxj57JvZkwOILwSl&p=other"
+  ></script>
+</svelte:head>
+
+<givebutter-widget {id}></givebutter-widget>
