90fcc62e4758e02b55c1f12ee35383c305093a84
Chris Amico <eyeseast@gmail.com>
2026-07-07 21:18:34 -0400

    Gate process loading on auth check

---
+++ src/lib/components/processing/tests/ProcessContext.demo.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { User } from "$lib/api/types";
+  import { setContext, untrack } from "svelte";
+  import { writable } from "svelte/store";
+
+  import ProcessContext from "../ProcessContext.svelte";
+
+  interface Props {
+    user?: User | null;
+  }
+
+  const props: Props = $props();
+
+  untrack(() => {
+    setContext("me", writable(props.user));
+  });
+</script>
+
+<ProcessContext />
