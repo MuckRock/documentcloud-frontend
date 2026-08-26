35d46aa8341be0fa538bcdeb1147535d809cb1bd
Daniel Nass <dnlnss@gmail.com>
2026-08-26 08:22:59 -0400

    Sync settings stores with localStorage in module block

--- src/lib/components/documents/CustomizeEmbed.svelte
+++ src/lib/components/documents/CustomizeEmbed.svelte
@@ -1,22 +1,20 @@
 <script lang="ts" module>
+  import { browser } from "$app/environment";
   import { writable } from "svelte/store";
   import { documentDefaults } from "$lib/utils/embedConfig";
+  import { saveStore } from "$lib/utils/storage";
 
   export const embedSettings = writable(documentDefaults);
+
+  if (browser) {
+    saveStore(embedSettings, "vieweroptions", { storage: localStorage });
+  }
 </script>
 
 <script lang="ts">
-  import { onMount } from "svelte";
-
   import Settings from "$lib/components/common/Settings.svelte";
 
   import { documentSettings } from "$lib/utils/embedConfig";
-  import { saveStore } from "$lib/utils/storage";
-
-  // initialize and sync settings with localStorage
-  onMount(() =>
-    saveStore(embedSettings, "vieweroptions", { storage: localStorage }),
-  );
 </script>
 
 <Settings settings={documentSettings} values={embedSettings} />
--- src/lib/components/projects/CustomizeProjectEmbed.svelte
+++ src/lib/components/projects/CustomizeProjectEmbed.svelte
@@ -1,22 +1,20 @@
 <script lang="ts" module>
+  import { browser } from "$app/environment";
   import { writable } from "svelte/store";
   import { projectDefaults } from "$lib/utils/embedConfig";
+  import { saveStore } from "$lib/utils/storage";
 
   export const embedSettings = writable(projectDefaults);
+
+  if (browser) {
+    saveStore(embedSettings, "projectoptions", { storage: localStorage });
+  }
 </script>
 
 <script lang="ts">
-  import { onMount } from "svelte";
-
   import Settings from "$lib/components/common/Settings.svelte";
 
   import { projectSettings } from "$lib/utils/embedConfig";
-  import { saveStore } from "$lib/utils/storage";
-
-  // initialize and sync settings with localStorage
-  onMount(() =>
-    saveStore(embedSettings, "projectoptions", { storage: localStorage }),
-  );
 </script>
 
 <Settings settings={projectSettings} values={embedSettings} />
