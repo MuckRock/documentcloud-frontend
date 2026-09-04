59e0000947a150930a7f05e10f36f052598b5743
Daniel Nass <dnlnss@gmail.com>
2026-09-04 16:30:17 -0400

    Add reusable infinite scroll trigger component

---
+++ src/lib/components/layouts/InfiniteScrollTrigger.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import type { APIError, Maybe } from "$lib/api/types";
+  import type { Attachment } from "svelte/attachments";
+  import { _ } from "svelte-i18n";
+  import Button from "$lib/components/common/Button.svelte";
+  import { SearchResultsState } from "$lib/state/search.svelte";
+
+  interface InfiniteScrollProps {
+    search: SearchResultsState<any>;
+    onNext?: () => Promise<Maybe<APIError<any>>>;
+  }
+
+  let { search, onNext }: InfiniteScrollProps = $props();
+
+  let error = $derived(search.error);
+  let onNextFn = $derived(onNext ?? search.loadNext);
+  let auto = $state(true);
+
+  const handleInfiniteScroll: Attachment = (trigger) => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(async (entry) => {
+        if (entry.isIntersecting && auto) {
+          const error = await onNextFn();
+          if (error) {
+            // don't keep trying if something fails
+            auto = false;
+          }
+        }
+      });
+    });
+
+    observer.observe(trigger);
+
+    return () => observer.disconnect();
+  };
+</script>
+
+<div {@attach handleInfiniteScroll}>
+  {#if search.loading && search.visible.size > 0}
+    {$_("common.loading")}
+  {:else if !auto && search.next}
+    <Button
+      ghost
+      mode="primary"
+      disabled={search.loading}
+      onclick={async () => {
+        error = await onNextFn();
+      }}
+    >
+      {$_("documents.more")}
+    </Button>
+  {/if}
+
+  {#if error}
+    <p class="error">{error.message}</p>
+    <p class="error">{$_("documents.retry")}</p>
+  {/if}
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
+    padding-bottom: 1rem;
+  }
+
+  .error {
+    text-align: center;
+    color: var(--error, red);
+  }
+</style>
