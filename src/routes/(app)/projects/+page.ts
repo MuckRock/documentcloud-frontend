e0834934389c0d00c433c05f6257a2f18cb0b3b4
Daniel Nass <dnlnss@gmail.com>
2026-09-04 16:42:38 -0400

    Add infinite scroll to projects list

--- src/routes/(app)/projects/+page.ts
+++ src/routes/(app)/projects/+page.ts
@@ -1,6 +1,3 @@
-import type { Maybe, ProjectResults } from "$lib/api/types";
-
-import { DEFAULT_PER_PAGE } from "@/config/config.js";
 import { list } from "$lib/api/projects";
 
 type ProjectFilter = "owned" | "shared" | "public";
@@ -8,49 +5,24 @@ type ProjectFilter = "owned" | "shared" | "public";
 export async function load({ url, parent, fetch }) {
   const { me } = await parent();
   const query = url.searchParams.get("query") ?? "";
-  const cursor = url.searchParams.get("cursor") ?? "";
-  const per_page = +(url.searchParams.get("per_page") ?? DEFAULT_PER_PAGE);
 
-  const params: Record<string, any> = {
-    query,
-    cursor,
-    per_page,
-  };
+  const params: Record<string, any> = { query };
 
   let filter: ProjectFilter =
     (url.searchParams.get("list") as ProjectFilter) ?? "owned";
 
-  if (!me) {
-    filter = "public";
-  }
-
-  const defaultProjects: ProjectResults = {
-    results: [],
-    next: null,
-    previous: null,
-  };
-
-  let data: Maybe<ProjectResults>;
-  let error: unknown;
-
   if (me && filter === "owned") {
-    ({ data, error } = await list({ ...params, owned_by_user: true }, fetch));
+    params.owned_by_user = true;
   }
 
   if (me && filter === "shared") {
-    ({ data, error } = await list({ ...params, is_shared: true }, fetch));
+    params.is_shared = true;
   }
 
-  if (filter === "public") {
-    ({ data, error } = await list(params, fetch));
-  }
+  const projects = list(params, fetch);
 
   return {
-    error,
-    list: filter,
-    projects: data ?? defaultProjects,
+    projects,
     query,
-    cursor,
-    per_page,
   };
 }
