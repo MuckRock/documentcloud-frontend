e35861baa51d4b796fe63a8cb848ee90d445de30
Daniel Nass <dnlnss@gmail.com>
2026-09-08 11:03:01 -0400

    Update squarelet staging URL

--- src/config/staging.js
+++ src/config/staging.js
@@ -2,6 +2,6 @@ export const DC_BASE = "https://api.staging.documentcloud.org";
 export const APP_URL =
   process.env.APP_URL || "https://www.staging.documentcloud.org";
 export const EMBED_URL = APP_URL;
-export const SQUARELET_BASE = "https://squarelet-staging.herokuapp.com";
+export const SQUARELET_BASE = "https://staging-accounts.muckrock.com";
 export const STAFF_ONLY_S3_URL =
   "https://s3.console.aws.amazon.com/s3/buckets/documentcloud-staging-files?region=us-east-1&prefix=documents/$$ID$$/&showversions=false";
