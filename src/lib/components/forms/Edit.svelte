5666328f652dabd644d6773dc9530814c872ed16
Daniel Nass <dnlnss@gmail.com>
2026-09-08 17:59:01 -0400

    Add applyEdits util for merging new values into edited store

--- src/lib/components/forms/Edit.svelte
+++ src/lib/components/forms/Edit.svelte
@@ -21,7 +21,7 @@ Usually this will be rendered inside a modal, but it doesn't have to be.
   import Text from "../inputs/Text.svelte";
   import TextArea from "../inputs/TextArea.svelte";
 
-  import { canonicalUrl, edited } from "$lib/api/documents";
+  import { applyEdits, canonicalUrl } from "$lib/api/documents";
   import { toDatetimeLocal } from "$lib/utils/date";
 
   interface Props {
@@ -65,12 +65,9 @@ Usually this will be rendered inside a modal, but it doesn't have to be.
 
       if (result.type === "success") {
         // save edits
-        edited.update((m) => {
-          const d = result.data.document;
-          d.access = formData.get("access"); // this is often a step behind, so force it here
-          m.set(String(d.id), d);
-          return m;
-        });
+        const d = result.data.document;
+        d.access = formData.get("access"); // this is often a step behind, so force it here
+        applyEdits(d);
         invalidate(`document:${document.id}`);
         onclose?.();
       }
--- src/lib/components/forms/EditAccess.svelte
+++ src/lib/components/forms/EditAccess.svelte
@@ -17,7 +17,7 @@ Usually this will be rendered inside a modal, but it doesn't have to be.
   import AccessLevel from "../inputs/AccessLevel.svelte";
   import Field from "../inputs/Field.svelte";
 
-  import { canonicalUrl, edited } from "$lib/api/documents";
+  import { applyEdits, canonicalUrl } from "$lib/api/documents";
   import { toDatetimeLocal } from "$lib/utils/date";
 
   interface Props {
@@ -60,12 +60,9 @@ Usually this will be rendered inside a modal, but it doesn't have to be.
 
       if (result.type === "success") {
         // save edits
-        edited.update((m) => {
-          const d = result.data.document;
-          d.access = formData.get("access"); // this is often a step behind, so force it here
-          m.set(String(d.id), d);
-          return m;
-        });
+        const d = result.data.document;
+        d.access = formData.get("access"); // this is often a step behind, so force it here
+        applyEdits(d);
         update(result);
         onclose?.();
       }
